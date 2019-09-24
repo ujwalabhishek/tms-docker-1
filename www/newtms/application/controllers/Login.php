52d543771f19f53b2df3539c6d5d5961f9976469
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-09-24 13:42:18 +0530

    debugging

--- www/newtms/application/controllers/Login.php
+++ www/newtms/application/controllers/Login.php
@@ -112,7 +112,7 @@ class Login extends CI_Controller {
                 $this->session->sess_destroy();
                 redirect('login/');
             } else {
-                redirect('Login/');
+                redirect('course_public/');
             }
         }
     }
