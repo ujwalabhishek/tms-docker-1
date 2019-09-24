6387e2802376a63344f93e82cd99f44ee6a056ba
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-09-24 10:07:56 +0530

    debugging

--- www/newtms/application/models/login_model.php
+++ www/newtms/application/models/login_model.php
@@ -3,7 +3,7 @@
   * This is the Model class for Login  
   */
 
-class Login_Model extends CI_Model {
+class login_model extends CI_Model {
     
     public function __construct() {
         parent::__construct();        
