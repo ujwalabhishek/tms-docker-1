52edd2b1ca75e1cc069dd44f134585130cd6cdc2
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-09-24 11:15:07 +0530

    debugging

--- www/newtms/application/controllers/Login.php
+++ www/newtms/application/controllers/Login.php
@@ -3,7 +3,7 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 class Login extends CI_Controller {
     public function __construct() {
-        parent::__construct();
+        parent::__construct();echo "ss1";exit;
         $this->load->model('login_model', 'login');
         $this->load->model('acl_model', 'acl');
         $this->load->library('bcrypt');
