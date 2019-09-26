bcab6b2a30c82cf0a28f5c29f22ef4fb3890f195
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-09-26 13:24:09 +0530

    internal user edit search issue

--- www/newtms/application/views/company/editcompany.php
+++ www/newtms/application/views/company/editcompany.php
@@ -96,7 +96,7 @@
             var self = $(this),
             button = self.find('input[type="submit"],button'),
             submitValue = button.data('submit-value');
-            button.attr('disabled','disabled').val('Please Wait..');
+            button.attr('disabled','disabled').html('Please Wait..');
             return true;
            }else{
                return false;
