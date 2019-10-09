4fcce99285db354a798af955a67f107c557a6c23
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-09 08:34:31 +0530

    login captcha refresh button

--- www/newtms/application/views/layout_1.php
+++ www/newtms/application/views/layout_1.php
@@ -176,10 +176,11 @@ span.psw {
                                                <div class='row'>
                                                    <div class='col-sm-6'>
                                                        <label for="psw"><b>Captcha Code</b></label>
-                                                        <div><?php echo $captcha;?>
+                                                        <div style='padding-top:2px'><?php echo $captcha;?>
                                                             <a href="/" title="Refresh">
-											<span class="glyphicon glyphicon-refresh" style="font-size: 20px;color: #486d90;"></span>
-										</a></div>
+                                                            &nbsp;<span class="glyphicon glyphicon-refresh" style="font-size: 20px;color: #486d90;font-weight:bold"></span>
+                                                            </a>
+                                                        </div>
                                                    </div>
                                                    <div class='col-sm-6'>
                                                        <label for="psw"><b>Enter Captcha Code</b></label>
