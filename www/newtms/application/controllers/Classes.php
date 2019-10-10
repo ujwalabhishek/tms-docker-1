2046b4264208eec0a33b98efa3e3849148cdd475
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-10 12:44:29 +0530

    direct enrolment sales executive id issue

--- www/newtms/application/controllers/Classes.php
+++ www/newtms/application/controllers/Classes.php
@@ -378,6 +378,7 @@ class Classes extends CI_Controller {
      * this function to update the edited class
      */
     public function update_class() { 
+        $data['sideMenuData'] = fetch_non_main_page_content();
         $tenant_id = $this->tenant_id;
         $user_id = $this->session->userdata('userDetails')->user_id;
         $role_id = $this->session->userdata('userDetails')->role_id;// added by shubhranshu for fetching role id
