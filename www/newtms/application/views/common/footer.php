d908519137cfff58cc9ffb879ec5aea920e0127f
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-11 13:44:38 +0530

    public portal new login page

--- www/newtms/application/views/common/footer.php
+++ www/newtms/application/views/common/footer.php
@@ -1,4 +1,4 @@
       <footer>
-        <p>&copy; <?php echo $this->data['tenant_details']->copyrighttext.', All Rights Reserved.'; ?><font style="font-size:9px; color:#ac0000; font-weight:bold; float:right;">powered by biipmi Pte Ltd.</font></p>
+        <p>Copyright &copy; <?php echo $this->data['tenant_details']->copyrighttext.', All Rights Reserved.'; ?><font style="font-size:9px; color:#ac0000; font-weight:bold; float:right;">powered by biipmi Pte Ltd.</font></p>
       </footer>
 	  
