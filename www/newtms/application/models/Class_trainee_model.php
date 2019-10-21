dc341640e4e778b28b7013b6a4cc2805fde28f9b
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-21 20:49:49 +0530

    debugging

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -3375,7 +3375,7 @@ public function company_enrollment_db_update($tenant_id, $loggedin_user_id, $com
                     'class_status' => $class_status,
                     'enrol_status' => $enrol_status
                 );
-                $this->db->trans_start();
+//                $this->db->trans_start();
                 $this->db->insert('class_enrol', $data);
                 if (!empty($payment_due_id)) 
                 {
@@ -3475,11 +3475,11 @@ public function company_enrollment_db_update($tenant_id, $loggedin_user_id, $com
         {
             $invoice_id = '';
         }
-        $this->db->trans_complete();
-        if ($this->db->trans_status() === FALSE) 
-        {
-            $status = FALSE;
-        }
+        //$this->db->trans_complete();
+//        if ($this->db->trans_status() === FALSE) 
+//        {
+//            $status = FALSE;
+//        }
         return array('err' => $err_arr, 'invoice' => $invoice_id, 'status' => $status, 'pymnt_due_id' => $payment_due_id);
     }    
     
