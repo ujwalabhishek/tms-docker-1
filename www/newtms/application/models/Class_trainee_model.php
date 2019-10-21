825665dff4abdb2c192652f5037685964ff8302d
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-21 13:00:04 +0530

    company enrollment more than one trainee

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -3378,7 +3378,7 @@ public function company_enrollment_db_update($tenant_id, $loggedin_user_id, $com
                 );
                 $this->db->trans_start();
                 $this->db->insert('class_enrol', $data);
-                
+                echo $this->db->last_query().'<br>';
                     
                 if (!empty($payment_due_id)) 
                 {
@@ -3397,7 +3397,7 @@ public function company_enrollment_db_update($tenant_id, $loggedin_user_id, $com
                         'att_status' => $att_status
                     );
                     $this->db->insert('enrol_pymnt_due', $data);
-                    
+                     echo $this->db->last_query().'<br>';
                 }
             } 
             else 
@@ -3474,7 +3474,7 @@ public function company_enrollment_db_update($tenant_id, $loggedin_user_id, $com
                 );
            // }
             $this->db->insert('enrol_invoice', $data);
-            
+             echo $this->db->last_query().'<br>';
         } 
         else 
         {
