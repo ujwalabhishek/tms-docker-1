c95c6c6c38da94cface8b658ff7b6b1c94b9d119
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-24 08:23:43 +0530

    landing page design

--- www/newtms/application/views/landing_page.php
+++ www/newtms/application/views/landing_page.php
@@ -32,6 +32,8 @@ echo $timezone = date_default_timezone_get() . " / " . date('m/d/Y h:i:s a', tim
 		
                 .header-ttl{color:#446cb3;
                 font-size: 40px;
+                    font-weight: 600;
+    text-shadow: 1px 1px 1px #161163;
                 font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, "sans-serif";
 		
                 }
@@ -109,8 +111,8 @@ echo $timezone = date_default_timezone_get() . " / " . date('m/d/Y h:i:s a', tim
                 <div class="container-fluid bg-4 text-center"> 
                     <div class="container bg-3 text-center">
                         <div class="row">
-                            <div class="col-sm-8"><h5  style="text-align: right;">Copyright © BIIPMI 2015-2019, All Rights Reserved.</h5></div>
-                            <div class="col-sm-4"><h6 style="text-align: right;">powered by biipmi Pte Ltd</h6></div>
+                            <div class="col-sm-8"><h5  style="text-align: right;color: #999393;">Copyright © BIIPMI 2015-2019, All Rights Reserved.</h5></div>
+                            <div class="col-sm-4"><h6 style="text-align: right;color: #999393;">powered by biipmi Pte Ltd</h6></div>
 
                         </div>
                     </div>
