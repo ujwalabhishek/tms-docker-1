a56e4cff0bf75455a5757522569c8a3b4f5e7178
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-24 08:55:46 +0530

    landing page design

--- www/newtms/application/views/common/login_header_public.php
+++ www/newtms/application/views/common/login_header_public.php
@@ -1,5 +1,29 @@
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.validate.js"></script> 
-<div class="container container_style">
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.validate.js"></script>
+
+<div class="container-fluid bg-3 text-center header-box">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2">
+               <a href="<?php echo base_url()?>">
+                <img class="logo" src="<?php echo base_url()?>logos/t01.png" border="0">
+                </a>
+            </div>
+            <div class="col-sm-8 header-ttl"><p>TRAINING MANAGEMENT SYSYTEM</p></div>
+            <div class="col-sm-2 header-ttl">
+                <a title="Join Now" href="<?php echo site_url(); ?>login/administrator" class="btn btn-sm btn-info"><strong style='color:white'>Admin Login<span class="glyphicon glyphicon-chevron-right"></span></strong> </a>
+                <a title="Join Now" href="<?php echo site_url(); ?>course_public/class_member_check" class="btn btn-sm btn-info"><strong style='color:white'>Trainee LogIn<span class="glyphicon glyphicon-chevron-right"></span></strong> </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+ 
+
+<!--<div class="container container_style">
     <div class="masthead">
         <h3 class="text-muted h3_logo"><a href="<?php echo site_url(); ?>" title="Home" >
                 <?php
@@ -41,7 +65,7 @@
                         </tr>  
                          <tr >
                             <td>
-<!--                                <a title="Enroll For Someone" href="<?php echo site_url(); ?>course/referral_credentials1" style="color: black; text-decoration: none"><strong>Enroll For Someone</strong>-->
+                                <a title="Enroll For Someone" href="<?php echo site_url(); ?>course/referral_credentials1" style="color: black; text-decoration: none"><strong>Enroll For Someone</strong>
                               
                             </td>
                            
@@ -54,12 +78,12 @@
                
                 
                
-<!--                <table width="100%" align="left"> <tr>
+                <table width="100%" align="left"> <tr>
                         <td rowspan="4" align="right" width="12%"> <img src="<?php echo base_url(); ?>assets/images/ref_frnd.png" align="left" /></td>
                         <td align="left" width="88%">
                             <a href="<?php echo base_url(); ?>course/referral_credentials">Enroll for Someone </a></td>
                     </tr> 
-                </table>-->
+                </table>
                 
                 
             </div>
@@ -69,4 +93,4 @@
             </a>
         <?php } ?>
     </div>	
-</div>
+</div>-->
--- www/newtms/application/views/landing_page.php
+++ www/newtms/application/views/landing_page.php
@@ -6,61 +6,63 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Biipmi Training Management System" />
-        <meta name="timezone" value="<?php date_default_timezone_set('Asia/Singapore');
-echo $timezone = date_default_timezone_get() . " / " . date('m/d/Y h:i:s a', time()); ?>">
+        <meta name="timezone" value="<?php date_default_timezone_set('Asia/Singapore');echo $timezone = date_default_timezone_get() . " / " . date('m/d/Y h:i:s a', time()); ?>">
         <title><?php echo $page_title; ?></title> 
-
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+        ga('create', 'UA-78372106-1', 'auto');
+        ga('send', 'pageview');
+
+      </script> 
         <style>
-                        #wrapper{
-                        margin:0;
-                        padding:0;
-                }
-		
-                .header-box{
-                        background-color: rgba(255, 255, 255, 0.2);
-                        -webkit-box-shadow: 0 3px 1px rgba(0, 0, 0, 0.2);
-                        padding: 20px;
-			
-                }
-                .header-div{margin:20px 0;}
-		
-		
-                .header-ttl{color:#446cb3;
+            #wrapper{
+                margin:0;
+                padding:0;
+            }
+
+            .header-box{
+                background-color: rgba(255, 255, 255, 0.2);
+                -webkit-box-shadow: 0 3px 1px rgba(0, 0, 0, 0.2);
+                padding: 20px;
+
+            }
+            .header-div{margin:20px 0;}
+
+            .header-ttl{color:#446cb3;
                 font-size: 40px;
-                    font-weight: 600;
-    text-shadow: 1px 1px 1px #161163;
+                font-weight: 600;
+                text-shadow: 1px 1px 1px #161163;
                 font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, "sans-serif";
-		
-                }
-		
-                .col-desg{
-                        padding:10px;
-                        height:100%;
-                        height:150px;
-                }
-	
-                .col-desg-tlt{
-                        background: #446cb3;
-                        padding: 10px;
-                        margin: 10px;
-                        height:40px;
-                        color: #fff;
-                        box-shadow: 0px -2px 6px 1px rgba(0, 0, 255, .2);
-                }
-		
-.col-desg-tlt:p{
-                        text-align: center;
-                        color: #fff;
-        margin-top: 30px;
-			
-                }
-		
-		
+
+            }
+
+            .col-desg{
+                padding:10px;
+                height:100%;
+                height:150px;
+            }
+
+            .col-desg-tlt{
+                background: #446cb3;
+                padding: 10px;
+                margin: 10px;
+                height:40px;
+                color: #fff;
+                box-shadow: 0px -2px 6px 1px rgba(0, 0, 255, .2);
+            }
+
+            .col-desg-tlt:p{
+                text-align: center;
+                color: #fff;
+                margin-top: 30px;	
+            }
 		
         </style>
 
@@ -118,7 +120,7 @@ echo $timezone = date_default_timezone_get() . " / " . date('m/d/Y h:i:s a', tim
                     </div>
                 </div>
             </div>
-
+       </div>
     </body>
 </html>
 
