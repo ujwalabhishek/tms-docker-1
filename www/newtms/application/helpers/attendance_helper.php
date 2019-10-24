004a3d6f4ceeeabdc9cfd3d697e9f390beee44f3
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-24 20:47:46 +0530

    attendance issue

--- www/newtms/application/helpers/attendance_helper.php
+++ www/newtms/application/helpers/attendance_helper.php
@@ -316,8 +316,8 @@ function calculate_start_end_date_range(DateTime $from_date, $to_date, $class_st
 
     }
     //echo date_default_timezone_get();
-    echo 1571025600-$week_start_time.'-------';
-print_r($week_start_time);exit;
+    //echo 1571025600-$week_start_time.'-------';
+print_r($week_start_time+604800);exit;
     if ($week == 1) {
 
         $week_start_time = strtotime("-7 days", $week_start_time);
