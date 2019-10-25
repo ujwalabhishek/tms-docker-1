9a2b34b284a7c5e0a63c23da1843504fa07b70bc
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-10-25 14:25:47 +0530

    NRIC field separation in trainee page

--- www/newtms/application/views/common/header.php
+++ www/newtms/application/views/common/header.php
@@ -19,7 +19,8 @@
                                 <td>
                                     <div class="welcome">Welcome <?php 
                                  $string = $this->session->userdata('userDetails')->first_name . ' ' . $this->session->userdata('userDetails')->last_name; 
-                                $fullname =(strlen($string) > 21) ? substr($string,0,17).'..' : $string;
+                                // added by shubhranshu
+                                 $fullname =(strlen($string) > 21) ? substr($string,0,17).'..' : $string;
                                  echo $fullname;
                                 ?></div>
                                 </td>
