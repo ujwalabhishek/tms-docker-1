f546931938bc0f962e786a00cea4cedac34c758a
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-11-07 10:13:38 +0530

    search invoice pdf

--- www/newtms/application/models/Reports_model.php
+++ www/newtms/application/models/Reports_model.php
@@ -1870,7 +1870,8 @@ SELECT  {$calc_rows} c.crse_name,
         }
         $this->db->group_by('ei.invoice_id');
         //return $this->db->get()->result_object();
-        $this->db->get();
+        $this->db->get()->result_object();
+        echo $this->db_last_query();
         echo memory_get_usage().'dd';exit;
     }
 
