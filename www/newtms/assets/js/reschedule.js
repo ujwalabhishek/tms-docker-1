5f0be228ee5c8113385e2db773c8c4877d7af0e2
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-11-11 09:59:40 +0530

    testing reschedule

--- www/newtms/assets/js/reschedule.js
+++ www/newtms/assets/js/reschedule.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
                 }
             } 
             else 
-            {alert($active_course+'t');
+            {
                 $(this).attr('disabled', 'disabled');
             }
             $i++;
