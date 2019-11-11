6abe88c27cfd9379a593075a3600c89f59fe7909
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-11-11 12:45:59 +0530

    testing reschedule

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -6308,8 +6308,8 @@ public function company_enrollment_db_update_backup($tenant_id, $loggedin_user_i
                 $status = $this->update_rescheduled_reason($tenant_id, $data['pymnt_due_id'], $trainee_id, $course_id, $prev_class_id, $reschedule_reason, $other_reason, $new_class_id);
             }
 
-            //if (count($temp_array) == 0 && $status) 
-            if (count($temp_array) == 0)
+            if (count($temp_array) == 0 && $status) 
+            //if (count($temp_array) == 0)
             {
 
                 $res = $this->reschedule_create_new_comp_enroll($tenant_id, $data, $course_id, $new_class_id, $trainee_id);
