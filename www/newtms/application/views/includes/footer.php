6f2df670cdbde1aa51c654811557004f2b837a33
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-11-13 15:06:23 +0530

    public portal issue

--- www/newtms/application/views/includes/footer.php
+++ www/newtms/application/views/includes/footer.php
@@ -8,7 +8,12 @@
     <div class="container-fluid bg-4 text-center"> 
         <div class="container bg-3 text-center">
             <div class="row">
-                <div class="col-sm-8"><h5  style="text-align: right;color: #999393;">Copyright © <?php echo $this->data['tenant_details']->copyrighttext; ?>, All Rights Reserved.</h5></div>
+                <div class="col-sm-8"><h5  style="text-align: right;color: #999393;">Copyright © <?php 
+                    if(!empty($this->data['tenant_details']->copyrighttext)){
+                       echo $this->data['tenant_details']->copyrighttext;
+                    }else{
+                    echo "BIIPMI 2015-2019";
+                    }?>, All Rights Reserved.</h5></div>, All Rights Reserved.</h5></div>
                 <div class="col-sm-4"><h6 style="text-align: right;color: #999393;">powered by biipmi Pte Ltd</h6></div>
 
             </div>
