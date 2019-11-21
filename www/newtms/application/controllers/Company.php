b280c9af8739acb4852cc0529bd2f13ae98a4c8f
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-11-21 10:20:18 +0530

    email checking

--- www/newtms/application/controllers/Company.php
+++ www/newtms/application/controllers/Company.php
@@ -944,7 +944,7 @@ class Company extends CI_Controller {
     'smtp_crypto' => 'ssl', //can be 'ssl' or 'tls' for example
     'mailtype' => 'html', //plaintext 'text' mails or 'html'
     'smtp_timeout' => '4', //in seconds
-    'charset' => 'iso-8859-1',
+    'charset' => 'utf-8',
     'wordwrap' => TRUE
 );
         $this->load->library('email', $config);
