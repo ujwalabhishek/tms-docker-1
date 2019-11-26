9ebbaa85a8fada7f76960dd31946feef1f375f0f
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-11-26 09:56:24 +0530

    credit note validation

--- www/newtms/application/config/email.php
+++ www/newtms/application/config/email.php
@@ -10,6 +10,18 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |*/
 
+//$config['protocol'] = 'smtp';
+//$config['smtp_host'] = 'server.biipmi.net';
+//$config['smtp_port'] = '25';
+//$config['smtp_user'] = 'enquiries@biipmi.co';
+//$config['smtp_pass'] = 'biipmisg2014';
+
+
+//$config['smtp_host'] = 'mail.bid4jeet.in';
+//$config['smtp_port'] = '587';
+//$config['smtp_user'] = 'test@bid4jeet.in';
+//$config['smtp_pass'] = 'dipu1234@@';
+
 $config['protocol'] = 'smtp';
 //$config['smtp_host'] = 'server.biipmi.net';
 //$config['smtp_port'] = '25';
@@ -17,10 +29,16 @@ $config['protocol'] = 'smtp';
 //$config['smtp_pass'] = 'biipmisg2014';
 
 
-$config['smtp_host'] = 'mail.bid4jeet.in';
-$config['smtp_port'] = '587';
-$config['smtp_user'] = 'test@bid4jeet.in';
-$config['smtp_pass'] = 'dipu1234@@';
+$config['smtp_host'] = 'ssl://smtp.gmail.com';
+$config['smtp_port'] = '465';
+$config['smtp_user'] = 'biipmisg@gmail.com';
+$config['smtp_pass'] = 'biipmiSG2015';
+
+$config['mailtype'] = 'html';
+$config['charset'] = 'utf-8';
+$config['newline'] = "\r\n";
+$config['smtp_timeout'] = 30;
+
 
 //$this->email->initialize($config);
 /* End of file email.php */
