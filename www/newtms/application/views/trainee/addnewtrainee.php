37be217cce0ef45e46579a720c5dfb2c546981a8
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-12-16 11:12:37 +0530

    new requirement for notaxcode trainee add page

--- www/newtms/application/views/trainee/addnewtrainee.php
+++ www/newtms/application/views/trainee/addnewtrainee.php
@@ -1269,7 +1269,7 @@ if (!empty($tax_error)) {
                 $('#SGP_OTHERS_label').text('OTHERS :');
                 $('#SGP_ID_label').text('');
                 $('#SGP_ID_label').text('OTHERS :');
-            }  else if (this.value == "SNG_4") {
+            }  else if (this.value == "SNG_4") {alert();
                 $("#SGP_OTHERS").show();
                 $('#SGP_OTHERS option:third-child').attr("selected", "selected");
                 $('#SGP_OTHERS_label').text('');
