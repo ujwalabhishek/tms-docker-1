6017bf04fd12da4340eeb4ad6e54b6aa7dfe667d
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-12-16 11:07:10 +0530

    new requirement for notaxcode trainee add page

--- www/newtms/application/views/trainee/addnewtrainee.php
+++ www/newtms/application/views/trainee/addnewtrainee.php
@@ -48,7 +48,7 @@ if (!empty($tax_error)) {
                                     foreach ($nrics as $item):
                                         $nris_options[$item['parameter_id']] = $item['category_name'];
                                     endforeach;
-                                    $nris_options['SNG_3'] = 'NO TAX CODE';
+                                    $nris_options['SNG_4'] = 'NO TAX CODE';
                                     $attr = 'id="NRIC"';
                                     echo form_dropdown('NRIC', $nris_options, $this->input->post('NRIC'), $attr);
                                     ?>
