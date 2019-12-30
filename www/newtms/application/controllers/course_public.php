b1bba7380dfe8f4c3d8bb9e3453a3fa9a12e71ad
biipbyte <info@biipbyte.com>
2019-12-31 01:46:19 +0530

    Changed db pwd to prod

--- www/newtms/application/controllers/course_public.php
+++ www/newtms/application/controllers/course_public.php
@@ -20,7 +20,7 @@ class Course_Public extends CI_Controller {
         $this->load->model('meta_values');
         $this->load->model('Manage_Tenant_Model', 'manage_tenant');
         $host=$_SERVER['HTTP_HOST'];
-        if($host != 'biipmi.co'){
+        if($host != 'biipmi.co' || $host != 'xprienzbakes.com'){
             $tenent_details = $this->course_model->get_tenant_details();
         }
         $this->session->set_userdata('public_tenant_details', $tenent_details);
@@ -37,7 +37,7 @@ class Course_Public extends CI_Controller {
         ////////////added by shubhranshu to show the landing page for all tenants////////////
 
         $host=$_SERVER['HTTP_HOST'];
-        if($host == 'biipmi.co'){
+        if($host == 'biipmi.co' || $host != 'xprienzbakes.com'){
             $data['page_title'] = 'BIIPMI Training Management Portal';
             $data['tenants'] = $this->manage_tenant->list_all_tenants_for_landing_page();
             $this->load->view('landing_page', $data);
@@ -3715,4 +3715,3 @@ class Course_Public extends CI_Controller {
 
      */
 
-    
