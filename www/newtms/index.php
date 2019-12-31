849e292ed6595fd3e915cb610cfd1df9e670e4cb
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-12-31 14:12:29 +0530

    check

--- www/newtms/index.php
+++ www/newtms/index.php
@@ -8,19 +8,33 @@
 //$username = "biipmico_tms";
 //$password = "ksj784382*879#prod!@2020";
 
-$link = mysqli_connect("127.0.0.1", "biipmico_tms", "ksj784382*879#prod!@2020", "biipmico_tms_masterdata");
+$mysqli = new mysqli("localhost", "biipmico_tms", "ksj784382*879#prod!@2020", "biipmico_tms_masterdata");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+$mysqli = new mysqli("127.0.0.1", "biipmico_tms", "ksj784382*879#prod!@2020", "biipmico_tms_masterdata", 3306);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+echo $mysqli->host_info . "\n";
+exit;
 
-mysqli_close($link);
+//$link = mysqli_connect("127.0.0.1", "biipmico_tms", "ksj784382*879#prod!@2020", "biipmico_tms_masterdata");
+//
+//if (!$link) {
+//    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//    exit;
+//}
+//
+//echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+//echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+//
+//mysqli_close($link);
 /**
  * CodeIgniter
  *
