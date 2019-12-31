3db5b60b8820efd41b2b19e70768c9fadd9b4cfe
Shubhranshu Panda <shubhranshu@biipbyte.com>
2019-12-31 14:02:00 +0530

    check

--- www/newtms/index.php
+++ www/newtms/index.php
@@ -4,21 +4,23 @@
 
 
 
-$servername = "127.0.0.1";
-$username = "biipmico_tms";
-$password = "ksj784382*879#prod!@2020";
+//$servername = "127.0.0.1";
+//$username = "biipmico_tms";
+//$password = "ksj784382*879#prod!@2020";
 
-// Create connection
-//$conn = new mysqli($servername, $username, $password,'biipmico_tms_masterdata');
+$link = mysqli_connect("127.0.0.1", "biipmico_tms", "ksj784382*879#prod!@2020", "biipmico_tms_masterdata");
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
-echo "Connected successfully";
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 /**
  * CodeIgniter
  *
