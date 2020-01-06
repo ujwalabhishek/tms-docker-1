957e01191bc622b02c149772df3b57a9292024c3
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-01-06 14:08:31 +0530

    COT Team requirement for class trainne non-masking report

--- www/newtms/application/helpers/export_helper.php
+++ www/newtms/application/helpers/export_helper.php
@@ -2437,7 +2437,7 @@ function export_classtrainee_full($result, $tenant_id,$userid) {
         $sheet->setCellValue('O' . $rn, !empty($row['certificate_coll_on']) ? date('d/m/Y',  strtotime($row['certificate_coll_on'])):'');
         $result_text = !empty($row['feedback_answer']) ? ' (Result: ' . $row['feedback_answer'].')' : '';
         $sheet->setCellValue('P' . $rn, $CI->class->get_class_status($row['class_id'], '').$result_text);
-        ////////COT TEAM REQUIREMENT CHANGED BY SHUBHRANSHU ON 06-01-2020 ////
+        ////////COT TEAM REQUIREMENT CHANGED BY SHUBHRANSHU ON 06-01-2020 (ONly Dhanush USERID NOT MASKING)////
         if($tenant_id =='T02' && $userid == '140490'){
            $sheet->setCellValue('Q' . $rn, $row['tax_code']); 
         }else{
