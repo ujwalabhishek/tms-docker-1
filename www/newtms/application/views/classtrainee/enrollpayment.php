a679da3ad369b089769aeb86408cf28c0859c7dd
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-01-29 12:07:16 +0530

    new requirement

--- www/newtms/application/views/classtrainee/enrollpayment.php
+++ www/newtms/application/views/classtrainee/enrollpayment.php
@@ -704,38 +704,50 @@ $discount_amount = ($classes->class_fees  * ($discount['discount_rate']/100) );
 <section class="mb-4">
 
     <!--Section heading-->
-    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Declaration Form</h2>
     <!--Section description-->
-    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
-        a matter of hours to help you.</p>
+    <p class="text-center w-responsive mx-auto mb-5">You must fill the form before to continue for the enrollment,I consent for Xprienz to collect and use my personal data for the purposes of and in accordance with Xprienz' privacy policy.</p>
 
     <div class="row">
 
         <!--Grid column-->
-        <div class="col-md-9 mb-md-0 mb-5">
+        <div class="col-md-12 mb-md-0 mb-5">
             <form id="contact-form" name="contact-form" action="mail.php" method="POST">
 
                 <!--Grid row-->
                 <div class="row">
 
                     <!--Grid column-->
-                    <div class="col-md-6">
+                    <div class="col-md-12">
                         <div class="md-form mb-0">
+                            <label for="name" class="">Your Name</label>
                             <input type="text" id="name" name="name" class="form-control">
-                            <label for="name" class="">Your name</label>
+                            
                         </div>
                     </div>
                     <!--Grid column-->
-
+                </div>
+                
+                <div class="row">
                     <!--Grid column-->
                     <div class="col-md-6">
                         <div class="md-form mb-0">
+                             <label for="email" class="">Your Email</label>
                             <input type="text" id="email" name="email" class="form-control">
-                            <label for="email" class="">Your email</label>
+                           
                         </div>
                     </div>
                     <!--Grid column-->
-
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                             <label for="email" class="">Your Mobile</label>
+                            <input type="text" id="email" name="email" class="form-control">
+                           
+                        </div>
+                    </div>
+                    <!--Grid column-->
+                    
                 </div>
                 <!--Grid row-->
 
@@ -743,8 +755,9 @@ $discount_amount = ($classes->class_fees  * ($discount['discount_rate']/100) );
                 <div class="row">
                     <div class="col-md-12">
                         <div class="md-form mb-0">
+                            <label for="subject" class="">Lesson Timing</label>
                             <input type="text" id="subject" name="subject" class="form-control">
-                            <label for="subject" class="">Subject</label>
+                            
                         </div>
                     </div>
                 </div>
@@ -757,8 +770,9 @@ $discount_amount = ($classes->class_fees  * ($discount['discount_rate']/100) );
                     <div class="col-md-12">
 
                         <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
                             <label for="message">Your message</label>
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            
                         </div>
 
                     </div>
@@ -774,23 +788,7 @@ $discount_amount = ($classes->class_fees  * ($discount['discount_rate']/100) );
         </div>
         <!--Grid column-->
 
-        <!--Grid column-->
-        <div class="col-md-3 text-center">
-            <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                    <p>San Francisco, CA 94126, USA</p>
-                </li>
-
-                <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                    <p>+ 01 234 567 89</p>
-                </li>
-
-                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                    <p>contact@mdbootstrap.com</p>
-                </li>
-            </ul>
-        </div>
-        <!--Grid column-->
+       
 
     </div>
 
