ed26256e03d296876fcec0da339e53049f4d6db6
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-01-30 12:37:48 +0530

    Testing commits

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -69,7 +69,8 @@ class Class_Trainee_Model extends CI_Model {
                                         'dec_lesson_timing' => $lesson_timing,
                                         'dec_overseas' => $overseas,
                                         'dec_type' => $type,
-                                        'dec_enrol_by' => $this->session->userdata('userDetails')->user_id ?? '0'
+                                        'dec_enrol_by' => $this->session->userdata('userDetails')->user_id ?? '0',
+                                        'dec_trigger_datetime' => date('Y-m-d H:i:s')
                                       );
         
     
