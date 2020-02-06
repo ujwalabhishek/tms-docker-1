02fc4171679424f8b0fc653c8d0762dd7403e629
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-02-06 10:53:54 +0530

    remove popup if the trainee assign to any compnay while direct reg

--- www/newtms/application/views/trainee/enroll_trainee_details.php
+++ www/newtms/application/views/trainee/enroll_trainee_details.php
@@ -683,7 +683,7 @@
 
 
 <!----------------modal by ssp start----------------------->
-<?php if ($this->session->userdata('userDetails')->tenant_id == 'T02'){$show='display:block';}else{$show='display:none';}?>
+<?php if ($this->session->userdata('userDetails')->tenant_id == 'T02' && $trainee_data->company_id =='0'){$show='display:block';}else{$show='display:none';}?>
 <div class="modal" id="ex1011" style="<?php echo $show;?>;margin:auto;margin-top:20px;margin-bottom:20px;">
 <p>
   <h2 class="panel_heading_style">Declaration</h2>
@@ -798,7 +798,7 @@
         
         
          //added by shubhranshu on 30 jan 2020 new declaration for trainee enrol 
-    <?php if ($this->session->userdata('userDetails')->tenant_id == 'T02'){?>
+    <?php if ($this->session->userdata('userDetails')->tenant_id == 'T02' && $trainee_data->company_id =='0'){?>
     $('#declra').hide();
     $('#declarations').click(function(){
         $status = 1;
