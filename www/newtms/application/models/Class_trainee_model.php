8fb135deefe33c459dd5d14681a993d53c58a48d
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-02-20 13:53:26 +0530

    testing

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -5403,7 +5403,7 @@ public function company_enrollment_db_update_backup($tenant_id, $loggedin_user_i
 
                 $brk_data = array(
                     'invoice_id' => $invoice_id,
-                    'refund_date' => date('Y-m-d H:i:S', strtotime($refund_date1)),
+                    'refund_date' => date('Y-m-d H:i:S', strtotime($refund_date)),
                     'user_id' => $trainee_id,
                     'refund_amount' => round($net_amount, 2),
                     'refund_by' => $user_id,
