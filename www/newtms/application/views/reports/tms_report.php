13a38cf82325c1f3a94b7286af4b9b244a78d2ea
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-02-25 11:00:43 +0530

    tms report new patch

--- www/newtms/application/views/reports/tms_report.php
+++ www/newtms/application/views/reports/tms_report.php
@@ -97,12 +97,12 @@
 
         <div class="bs-example">
             <div class="table-responsive">
-                <!--            <div class="add_button space_style">
-                <?php //if (count($tabledata) > 0 && array_key_exists('EXP_XLS', $this->data['left_side_menu']['INTUSR'])) { ?>
-                                            <a href="<?php //echo site_url('/internal_user/export_users_page' . $export_url) ?>"  class="small_text1"><span class="label label-default black-btn"><span class="glyphicon glyphicon-export"></span> Export Page Fields</span></a> &nbsp;&nbsp;
+                            <div class="add_button space_style">
+                <?php if (count($result) > 0 && array_key_exists('EXP_XLS', $this->data['left_side_menu']['INTUSR'])) { ?>
+                                           
                                             <a href="<?php //echo site_url('/internal_user/export_users_full' . $export_url) ?>"  class="small_text1"><span class="label label-default black-btn"><span class="glyphicon glyphicon-export"></span> Export All Fields</span></a>
-                <?php //} ?>
-                            </div>-->
+                <?php } ?>
+                            </div>
                 <div style="clear:both;"></div>
                 <table id="listview" class="table table-striped">
                     <thead>
