ac37321771c31b7e9f963460915d1b3febd90cee
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-02-25 14:26:15 +0530

    tms report new patch

--- www/newtms/application/helpers/export_helper.php
+++ www/newtms/application/helpers/export_helper.php
@@ -11,9 +11,9 @@ function export_tms_report_page($result) {
         $CI->excel->getActiveSheet()->getColumnDimension($columnID)
                 ->setAutoSize(true);
     }
-    $sheet->mergeCells('A1:S1');
+    $sheet->mergeCells('A1:G1');
     $sheet->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-    $sheet->setCellValue('A1', 'TMS '.$result[0]->payment_status.'Report List as on ' . date('M j Y, l'));
+    $sheet->setCellValue('A1', 'TMS '.$result[0]->payment_status.' Report List as on ' . date('M j Y, l'));
     $sheet->getStyle('A1:G1')->getFont()->setBold(true);
     $sheet->setCellValue('A2', 'SL #');
     $sheet->setCellValue('B2', 'NRIC/FIN No.');
