ddafa593972da1f1a46891655ce7fd59c04000c5
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-02-26 13:06:16 +0530

    tms report new patch

--- www/newtms/application/views/reports/tms_report.php
+++ www/newtms/application/views/reports/tms_report.php
@@ -217,7 +217,7 @@
     </div>    
    
 
-        <div class="bs-example" style='display:<?php $_POST['mVal'] ? 'none': 'block';?>'>
+        <div class="bs-example" style='display:<?php echo ($_POST['mVal'] ? 'none': 'block');?>'>
             <div class="table-responsive">
                             <div class="add_button " style='margin-top: 6px;'>
                 <?php if (count($result) > 0 && array_key_exists('EXP_XLS', $this->data['left_side_menu']['INTUSR'])) { ?>
@@ -295,8 +295,8 @@
                                 
                             </tr>
                         <?php } 
-                      
-                        echo $text.$unpaidVal;
+                            
+                        echo ($text ? $text : '').($unpaidVal ? $unpaidVal : '');
                         
                         ?>
                     </tbody>      
