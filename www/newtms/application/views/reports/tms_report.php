fa0424d6e41b7fff759b793993ae80d3ad7fd4c4
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-02-26 08:09:14 +0530

    tms report new patch

--- www/newtms/application/views/reports/tms_report.php
+++ www/newtms/application/views/reports/tms_report.php
@@ -4,6 +4,7 @@
     $baseurl = '<?php echo base_url(); ?>';
 </script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <div class="col-md-10">
     <?php
     $CI = & get_instance();
