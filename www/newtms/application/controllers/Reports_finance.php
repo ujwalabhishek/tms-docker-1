202017fbd4f3098f40f76b325d7142da3689096f
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-03-12 14:02:04 +0530

    company search by invoice number

--- www/newtms/application/controllers/Reports_finance.php
+++ www/newtms/application/controllers/Reports_finance.php
@@ -1540,6 +1540,6 @@ class Reports_finance extends CI_Controller {
     public function tms_report_search_company_name(){
        $invoice_no = $this->input->post('invoice_no'); 
        $res = $this->reportsModel->fetch_company_name_by_invoice_id($invoice_no);
-       echo json_decode($res);
+       echo json_encode($res);
     }
 }
