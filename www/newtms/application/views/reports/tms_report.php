4fe5cdbb2dba88987259e77191f141362cd05b5b
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-03-12 14:44:52 +0530

    company search by invoice number

--- www/newtms/application/views/reports/tms_report.php
+++ www/newtms/application/views/reports/tms_report.php
@@ -373,7 +373,7 @@ echo form_dropdown('tStatus', $s_options, $this->input->post('tStatus'), $attr);
             }
             
             if ($status){
-                $(".comp_block").slideUp("slow"); 
+                $(".comp_block").slideUp("fast"); 
                 $('.comp_block').html('');
                 $('.comp_search_button1').attr('disabled', 'disabled').html('Please Wait..');
                 $.ajax({
