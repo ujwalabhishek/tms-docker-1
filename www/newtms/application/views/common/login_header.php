a9b2e6fa5f7814415baa1cd26c89bab322c0fe03
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-03-16 12:31:42 +0530

    cbld logo

--- www/newtms/application/views/common/login_header.php
+++ www/newtms/application/views/common/login_header.php
@@ -24,7 +24,7 @@
         <div class="row">
             <div class="col-sm-2">
                <a href="<?php echo base_url()?>" class='pull-left'>
-                <img class="logo" src="<?php echo base_url()?>logos/t01.png" border="0">
+                <img class="logo" src="<?php echo base_url()?>logos/<?php echo TENANT_LOGO;?>" border="0">
                 </a>
             </div>
             <div class="col-sm-7 header-ttl"><p>TRAINING MANAGEMENT SYSTEM</p></div>
