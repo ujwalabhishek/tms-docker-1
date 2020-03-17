ad849857c86f0645f2564dd0eb16f339fe6c756c
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-03-17 15:32:13 +0530

    search company invoice

--- www/newtms/application/views/reports/search_company.php
+++ www/newtms/application/views/reports/search_company.php
@@ -95,6 +95,7 @@ $(document).ready(function() {
                         $(".comp_block").slideDown("slow");
                         $(".comp_status").slideDown("slow");
                         $(".comp_head").slideDown("slow");
+                        $(".comp_status").html(data.status);
                         $('.comp_search_button1').removeAttr('disabled').html('<span class="glyphicon glyphicon-search"></span> Search');
                     }
                 });
