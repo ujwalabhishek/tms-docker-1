2b0ed1f362b22741e12473509e1eda4e30ed5bda
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-03-27 09:06:25 +0530

    email diagnose

--- www/newtms/application/config/email.php
+++ www/newtms/application/config/email.php
@@ -10,7 +10,7 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |*/
 
-$config['protocol'] = 'smtp';
+$config['protocol'] = 'sendmail';
 //$config['smtp_host'] = 'ssl://mail.xprienz.net';
 //$config['smtp_port'] = '465';
 //$config['smtp_user'] = 'noreply@xprienz.net';
