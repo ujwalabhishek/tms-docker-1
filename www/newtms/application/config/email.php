8f78cd4296dadaadcc690925e5374c19433b0b20
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-03-30 07:56:33 +0530

    email diagnose

--- www/newtms/application/config/email.php
+++ www/newtms/application/config/email.php
@@ -10,7 +10,7 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |*/
 
-$config['protocol'] = 'smtp';
+$config['protocol'] = 'sendmail';
 //$config['smtp_host'] = 'ssl://mail.xprienz.net';
 //$config['smtp_port'] = '465';
 //$config['smtp_user'] = 'noreply@xprienz.net';
@@ -27,7 +27,7 @@ $config['protocol'] = 'smtp';
 //$config['smtp_user'] = 'biipmisg2020@gmail.com';
 //$config['smtp_pass'] = 'biipmisupport@123';
 
-$config['smtp_host'] = 'server01.thatsithosting.com';
+$config['smtp_host'] = 'ssl://server01.thatsithosting.com';
 $config['smtp_port'] = '465';
 $config['smtp_user'] = 'support@biipmi.co';
 $config['smtp_pass'] = 'BiipmiSG@2020';
