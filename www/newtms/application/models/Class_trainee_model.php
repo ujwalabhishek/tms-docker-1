aadd4cf7c6e4aa503e86686ebd9790c83df2062d
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-14 12:00:36 +0530

    new requirement everest

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -4502,12 +4502,13 @@ public function company_enrollment_db_update_backup($tenant_id, $loggedin_user_i
                 //return ($feesdue + (($feesdue * $gst_rate) / 100));
                 $feesduetemp = ($feesdue + (($feesdue * $gst_rate) / 100));
                  return $feesduetemp.'-'.$feesdue.'-'.$gst_rate.'-'.$subsidy.'-';
-                return round($feesduetemp, 2, PHP_ROUND_HALF_UP);/////ssp/////
+                //return round($feesduetemp, 2, PHP_ROUND_HALF_UP);/////ssp/////
             } else {
                 // added by shubhranshu to fixed calculation issue while round off upto 2decimal places on 5/12/2018/////
                  $feesduetemp = ($feesdue - $subsidy);
                // return $feesduetemp = ($feesdue - $subsidy);
-                return round($feesduetemp, 2, PHP_ROUND_HALF_UP);///////////////////ssp/////////////
+                 return $feesduetemp.'-'.$feesdue.'-'.$gst_rate.'-'.$subsidy.'-';
+                //return round($feesduetemp, 2, PHP_ROUND_HALF_UP);///////////////////ssp/////////////
             }
         } else {
             $feesduetemp=($feesdue - $subsidy);
