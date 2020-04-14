559037e413af0ab1728a9cb38b16abcab6f927f2
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-14 12:09:33 +0530

    new requirement everest

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -4500,20 +4500,22 @@ public function company_enrollment_db_update_backup($tenant_id, $loggedin_user_i
             if ($subsidy_after_before == 'GSTBSD') {
 
                 //return ($feesdue + (($feesdue * $gst_rate) / 100));
-                $feesduetemp = ($feesdue + (($feesdue * $gst_rate) / 100));
-                //return round($feesduetemp, 2, PHP_ROUND_HALF_UP);/////ssp/////
+                $feesduetemp = ($feesdue + (($feesdue * $gst_rate) / 100));return '44';
+                return round($feesduetemp, 2, PHP_ROUND_HALF_UP);/////ssp/////
             } else {
                 // added by shubhranshu to fixed calculation issue while round off upto 2decimal places on 5/12/2018/////
                  $feesduetemp = ($feesdue - $subsidy);
                // return $feesduetemp = ($feesdue - $subsidy);
-                //return round($feesduetemp, 2, PHP_ROUND_HALF_UP);///////////////////ssp/////////////
+                 return '456';
+                return round($feesduetemp, 2, PHP_ROUND_HALF_UP);///////////////////ssp/////////////
             }
         } else {
             $feesduetemp=($feesdue - $subsidy);
             //return ($feesdue - $subsidy);
-            //return round($feesduetemp, 2, PHP_ROUND_HALF_UP);
+            return '66';
+            return round($feesduetemp, 2, PHP_ROUND_HALF_UP);
         }
-        return $feesduetemp;
+        return $feesduetemp.'-'.$feesdue.'-'.$gst_rate.'-'.$subsidy.'-';
     
     }
 
