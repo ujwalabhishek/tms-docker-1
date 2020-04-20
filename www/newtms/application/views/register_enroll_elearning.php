c391171f81ca93ada2054e83c69275369ae691a3
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-20 10:15:55 +0530

    xp new elearning thing

--- www/newtms/application/views/register_enroll_elearning.php
+++ www/newtms/application/views/register_enroll_elearning.php
@@ -132,7 +132,7 @@ if($course_id!='' && $class_id!=''){
 
     <div id ='trainee_validation_div' class='reg_tbl_div' style='width:80%'>
         <div class="bs-example">                    
-                    <div class="table-responsive">
+                    <div class="table-responsive1">
                         
     
     
