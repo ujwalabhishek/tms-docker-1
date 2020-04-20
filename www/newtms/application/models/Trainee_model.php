ff55d3c2b31ae4040e56348b7298c090f303bc21
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-20 15:48:17 +0530

    xp new elearning thing

--- www/newtms/application/models/Trainee_model.php
+++ www/newtms/application/models/Trainee_model.php
@@ -702,7 +702,6 @@ public function get_training_details($user_id = NULL, $limit = NULL, $offset = N
         $this->db->where('usr.user_id', $user_id);
         $this->db->where('usr.tenant_id', $tenantid);
         $query = $this->db->get();
-        echo $this->db->last_query();exit;
         $result = $query->result_array();
         $data[userdetails] = $result[0];
         $userid = $data[userdetails][user_id];
