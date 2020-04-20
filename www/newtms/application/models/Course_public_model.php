458b8f0ea008b8ccbcb15241264b99630a993828
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-20 11:34:50 +0530

    xp new elearning thing

--- www/newtms/application/models/Course_public_model.php
+++ www/newtms/application/models/Course_public_model.php
@@ -5891,7 +5891,7 @@ class Course_Public_Model extends CI_Model {
         $data = $sql->row();
 
         if ($sql->num_rows() > 0) {
-            echo json_encode($data);
+            echo $data;
            
         } else {
 
