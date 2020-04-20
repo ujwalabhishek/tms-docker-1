1775cf3f79be3b6c00d707ce855833603d4b495a
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-20 10:39:30 +0530

    xp new elearning thing

--- www/newtms/application/views/register_enroll_elearning.php
+++ www/newtms/application/views/register_enroll_elearning.php
@@ -173,7 +173,7 @@ if($course_id!='' && $class_id!=''){
                                 </td>
                              
                             </tr>
-                            <tr> <td colspan="2" class='text-center'><button type='button' class='btn btn-primary btn-lg'>Submit</button></td><tr>
+                            <tr> <td colspan="2" class='text-center'><button type='button' class='btn-lg btn btn-primary'>Submit</button></td><tr>
                            
 
 
