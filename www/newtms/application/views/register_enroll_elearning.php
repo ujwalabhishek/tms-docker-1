27530150fb37fe3111bd9b2d2701803b9948c5da
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-21 14:10:59 +0530

    xp new elearning thing

--- www/newtms/application/views/register_enroll_elearning.php
+++ www/newtms/application/views/register_enroll_elearning.php
@@ -15,13 +15,13 @@ echo $this->load->view('common/refer_left_wrapper');
 
 </script>
 
-
+<!--
 <div class="col-md-2 col_2_style">
     <ul class="ad">
         <li><a href="#" class="thumbnail"><img src="<?php echo base_url();?>/assets/images/ad1.jpg" style="display: block;" data-src="holder.js/100%x180" alt="100%x180"></a></li>
         <li><a href="#" class="thumbnail"><img src="<?php echo base_url();?>/assets/images/ad2.jpg" style="display: block;" data-src="holder.js/100%x180" alt="100%x180"></a></li>
     </ul>
-</div>
+</div>-->
 
 
 <div class="col-md-10" style='font-size: 16px;'>  
