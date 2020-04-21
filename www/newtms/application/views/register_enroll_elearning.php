5a2dcda7bc2c4072728ec5c296bd6a90a858ad97
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-21 10:57:17 +0530

    xp new elearning thing

--- www/newtms/application/views/register_enroll_elearning.php
+++ www/newtms/application/views/register_enroll_elearning.php
@@ -367,6 +367,8 @@ if($course_id!='' && $class_id!=''){
     $(document).ready(function(){
         $('.nocls').click(function(){
              $('#modal_nric_found').hide();
+             document.trainee_form2.action = "course_public/referral_credentials1";
+              //$('#trainee_form2').submit();
         });
         $('.yescls').click(function(){
              $('#modal_nric_found').hide();
