a09d13f0b9a60398ee7bb991aa1ec9be9ca905eb
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-04-22 10:55:56 +0530

    xp new elearning thing

--- www/newtms/application/views/course_public/edit_trainee_details.php
+++ www/newtms/application/views/course_public/edit_trainee_details.php
@@ -66,7 +66,7 @@ if (!empty($tax_error)) {
 
                                </label></td>
                            <td class="td_heading">Discount@ : <label class="label_font">&nbsp;&nbsp;$
-                               <?php echo number_format($discount_total, 2, '.', ''); ?>%</td>
+                               <?php echo $discount_total;?>%</td>
                        </tr>
 
                        <tr style="display:none;">
