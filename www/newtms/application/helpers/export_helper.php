cbc76e007a9ffbeed25d6f29ea4ad281d0817d0c
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-05-12 15:08:45 +0530

    new requirement

--- www/newtms/application/helpers/export_helper.php
+++ www/newtms/application/helpers/export_helper.php
@@ -2928,9 +2928,9 @@ $CI->excel->getActiveSheet()->getColumnDimension($var . $columnID)
         $assessor_text = '';
         foreach ($assessor as $assess) {
             $assessor_text .= $assess->tax_code . ', ';
-            //break;
+            break;
         }
-        echo $assessor_text;exit;
+        //echo $assessor_text;exit;
         $assessor_text = rtrim($assessor_text, ', ');
         $strlength = strpos($row->tax_code_type, '_');
         $tax_code_type = empty($strlength) ? $row->tax_code_type : substr($row->tax_code_type, $strlength + 1);
