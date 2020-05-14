8d2fba303a8a6400c7365e70c65416b7dd152114
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-05-14 11:47:39 +0530

    new requirement

--- www/newtms/assets/public_js/validation_register_enroll.js
+++ www/newtms/assets/public_js/validation_register_enroll.js
@@ -1685,7 +1685,7 @@ function remove_image() {
 
 }
 
-$('.nric_submit').click(function(){
+$('.nric_submit').click(function(){alert();
     e = $('#taxcode_nric').val();
     if(e == ''){
         $('#taxcode_nric_err').text('[Required!]').addClass('error');
