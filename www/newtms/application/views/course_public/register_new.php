8587172ca225912e463562e640690b2c6b56025d
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-05-14 10:33:26 +0530

    new requirement

--- www/newtms/application/views/course_public/register_new.php
+++ www/newtms/application/views/course_public/register_new.php
@@ -3,9 +3,8 @@ $this->load->helper('metavalues_helper');
 
 $this->load->helper('common_helper');
 
-echo $this->load->view('common/refer_left_wrapper');
+echo $this->load->view('common/refer_left_wrapper_public');
 
-//echo validation_errors('<div class="error1">', '</div>');
 ?>
 <script>
 
@@ -17,118 +16,33 @@ echo $this->load->view('common/refer_left_wrapper');
 
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/public_js/enrol_some_one.js"></script>
 
-
-
-<div class="ref_col ref_col_tax_code">  
-    <h2 class="panel_heading_style">
-        <span aria-hidden="true" class="glyphicon glyphicon-user"></span> 
-          <?php if(!empty($user_id)){?> Enroll For Someone<?php }else{?> Registration Form <?php }?>
-
-    </h2>
+    <h2 class="panel_heading_style"><span aria-hidden="true" class="glyphicon glyphicon-user"></span>Registration Form</h2>
     <?php
 
     if (!empty($error_message)) {
-                    echo '<div style="color:red;font-weight: bold;">' . $error_message . '</div>';
-                }
+        echo '<div style="color:red;font-weight: bold;">' . $error_message . '</div>';
+        }
     if ($this->session->flashdata('error')) {
-                    echo '<div style="color:red;font-weight: bold;">
-                            ' . $this->session->flashdata('error') . '
-                        </div>';
-                }
+        echo '<div style="color:red;font-weight: bold;">
+                ' . $this->session->flashdata('error') . '
+            </div>';
+        }
     ?>
-<!--    <div class="tax_col">
-</div>-->
 
     <?php 
-            $atr = 'id="trainee_form" name="trainee_form" onsubmit="return(validate(\'refer_form\'));"';
-           // echo form_open_multipart("course/enrol_once", $atr);
-            echo form_open_multipart("user/add_new_trainee", $atr);
-           //  echo form_open_multipart("user/add_trainee1", $atr);
-             $user_id;
-            echo form_hidden('r_user_id', $user_id);
-            if(!empty($user_id)){
-                 echo form_hidden('loggedin', 1);
-            }
-            
-            echo form_hidden('country_of_residence', 'SGP');
-            echo form_hidden('course_id', $course_id);
-            echo form_hidden('class_id', $class_id);
-            echo form_hidden('registration', '1');
-            
+        $atr = 'id="trainee_form" name="trainee_form" onsubmit="return(validate(\'refer_form\'));"';
+        echo form_open_multipart("user/add_new_trainee", $atr);
+        echo form_hidden('r_user_id', $user_id);
+        if(!empty($user_id)){
+             echo form_hidden('loggedin', 1);
+        }
+
+        echo form_hidden('country_of_residence', 'SGP');
+        echo form_hidden('course_id', $course_id);
+        echo form_hidden('class_id', $class_id);
+        echo form_hidden('registration', '1');
     ?>  
-<?php
-if($course_id!='' && $class_id!=''){
-   
-?>
-<div style="color:black;font-weight: bold; padding: 6px;text-align:center;" class="reg_tbl_div">                                
-    <table class="table table-striped" style="">
-                 <tbody>
-                    <tr>
-                        <td  class="td_heading">Class Name: 
-                            <label class="label_font"><?php echo $class_details->class_name; ?></label>&nbsp;&nbsp;&nbsp;
-                               <a href="#ex12" rel="modal:open" class="small_text1"> <label class="label_font" style="cursor: pointer;">View Details</label>
-                            </a>
-                        </td>
-                        <td class="td_heading">Unit Fees: <label class="label_font">&nbsp;&nbsp;$&nbsp;<?php echo number_format($class_details->class_fees, 2, '.', ''); ?>
-                            
-                            </label></td>
-                        <td class="td_heading">Discount@ : <label class="label_font">&nbsp;&nbsp;$
-                            <?php echo number_format($class_details->class_discount, 2, '.', ''); ?>%</td>
-                    </tr>
-                
-                    <tr style="display:none;">
-                     
-                        <td class="td_heading" ><?php echo $gst_label; ?>:<label class="">$ <?php echo number_format($totalgst, 2, '.', ''); ?></label> </td>
-                        <td colspan="2" class="td_heading">Net Fee: <label class=""><?php echo '$ ' . number_format($net_due, 2, '.', ''); ?></label></td>
-                        
-                    </tr>  
-                    <tr>
-                     
-                     <td class="td_heading" ><?php echo $gst_label; ?>: <label class="label_font">$ <?php echo number_format($totalgst, 2, '.', ''); ?></label></td>
-                     <td colspan="2" class="td_heading">Net Fee: <label class="label_font"><?php echo '$ ' . number_format($net_due, 2, '.', ''); ?></label></td>
-                        
-                    </tr>  
-                   
-                </tbody>
-    </table>
-</div>
 
-<div class="modalnew modal13" id="ex12" style="display:none;height:280px !important; min-height: 280px !important;">
-      <h2 class="panel_heading_style" style="margin-bottom: -3px !important;">Class Details </h2>
-                <div class="class_desc_course">
-                    <div class="table-responsive">
-                        <table class="table table-striped">     
-                            <tr>
-                                <td width="40%"><span class="crse_des">Course Name :</span></td>
-                                <td><?php echo   $course_details->crse_name;?></td>
-                            </tr>
-                            <tr>
-                                <td><span class="crse_des">Class Name :</span></td>
-                                <td><?php echo $class_details->class_name; ?></td>
-                            </tr>
-                            <tr>
-                                <td><span class="crse_des">Class Start Date and Time :</span></td>
-                                <td><?php echo date('d/m/Y h:i A', strtotime($class_details->class_start_datetime)); ?></td>
-                            </tr>
-                            <tr>
-                                <td><span class="crse_des">Class End Date and Time :</span></td>
-                                <td><?php echo  date('d/m/Y h:i A', strtotime($class_details->class_end_datetime)); ?></td>
-                            </tr>
-                            <tr>
-                                <td><span class="crse_des">Classroom Location :</span></td>
-                                <td><?php echo $classloc; ?></td>
-                            </tr>
-                            
-
-                        </table>
-                    </div>                                
-                </div>
-                <div class="popup_cancel11">
-                    <a href="#" rel="modal:close"><button class="btn btn-primary" type="button">Close</button></a>
-                </div>
-            </div>  
-
-<?php } ?>
 
     <div id ='trainee_validation_div' class='reg_tbl_div'>
         <div class="bs-example">                    
@@ -550,13 +464,8 @@ if($course_id!='' && $class_id!=''){
             </div>
         
      <br/><br/>
-     
        
-     
-     <?php // echo form_close(); ?>   
-    </div>
-
-<br/><br/>
+  
 
 <!----------------modal by ssp start----------------------->
 <?php if (TENANT_ID == 'T02'){$show='display:block';}else{$show='display:none';}?>
