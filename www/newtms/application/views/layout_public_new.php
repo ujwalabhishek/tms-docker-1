57963dd37472b20dfed48634de8463c2955ceb3b
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-05-14 14:27:46 +0530

    new requirement

--- www/newtms/application/views/layout_public_new.php
+++ www/newtms/application/views/layout_public_new.php
@@ -9,20 +9,16 @@
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
-        <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>-->
+       
         <script> var baseurl = "<?php print base_url(); ?>";
       </script>
       <script src="<?php echo base_url(); ?>assets/public_js/date_time.js" type="text/javascript" charset="utf-8"></script>
-      <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
       <link href="<?php echo base_url(); ?>assets/public_css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
       <link href="<?php echo base_url(); ?>assets/public_css/style.css" rel="stylesheet" type="text/css">
-<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
       <link href="<?php echo base_url(); ?>assets/public_css/glyphicons.css" rel="stylesheet" type="text/css">
-<link href="<?php echo base_url(); ?>assets/public_css/autocomplete.css" rel="stylesheet" type="text/css"  />
-<link href="<?php echo base_url(); ?>assets/public_css/student.css"  rel="stylesheet" type="text/css" media="screen" />
+        <link href="<?php echo base_url(); ?>assets/public_css/autocomplete.css" rel="stylesheet" type="text/css"  />
+        <link href="<?php echo base_url(); ?>assets/public_css/student.css"  rel="stylesheet" type="text/css" media="screen" />
 
         <style>
             #wrapper{
