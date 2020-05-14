5070ccb176f2873e50cb62e0c8b01f2bc9227761
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-05-14 11:06:22 +0530

    new requirement

--- www/newtms/application/views/layout_public_new.php
+++ www/newtms/application/views/layout_public_new.php
@@ -9,7 +9,7 @@
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <?php $this->load->view('common/includes_public'); ?>
+        <?php //$this->load->view('common/includes_public'); ?>
         <style>
             #wrapper{
                 margin:0;
