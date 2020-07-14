820bb9764d300536fd6b0ddbc1ce3afdca57ed84
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-07-14 14:55:07 +0530

    sales report

--- www/newtms/application/config/tms_routes.php
+++ www/newtms/application/config/tms_routes.php
@@ -75,6 +75,7 @@ $tms_route['RPTFIN']['ops'] = array(
     'SLASMNTCOM' => 'sales',
     'ACTLOG' => 'activity_log',
     'TMSRPTS' => 'tms_report',
+    'SALREP' => 'sales_report',
     'INVADTRAIL' => 'invoice_audit_trail'); //Added for new Invoice Audit Trail Report
 //------------------------ Manage Tenant -------------------------------------
 $tms_route['MNGTENT']['controller_name'] = 'manage_tenant';
