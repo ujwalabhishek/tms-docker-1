4182c49fb6f84fb9c4c51258390e067eaf958c7f
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-08-05 15:52:39 +0530

    ssg api integration

--- www/newtms/application/controllers/Ssgapi_course.php
+++ www/newtms/application/controllers/Ssgapi_course.php
@@ -59,7 +59,7 @@ class ssgapi_course extends CI_Controller {
         CURLOPT_TIMEOUT => 0,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_CUSTOMREQUEST => "POST",
         CURLOPT_HTTPHEADER => array(
        "Authorization: c0d3cf1102b248a097846d7232d6ad8f",
        "Cache-Control: no-cache"
