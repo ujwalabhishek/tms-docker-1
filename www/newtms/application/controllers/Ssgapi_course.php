47dbabd2f11efc896898c093441c8fbee9cd105c
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-08-05 15:17:23 +0530

    ssg api integration

---
+++ www/newtms/application/controllers/Ssgapi_course.php
@@ -0,0 +1,46 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*
+ * his is the controller class for Accounting Use Cases
+ */
+
+class ssgapi_course extends CI_Controller {
+
+    private $user;
+
+    /**
+     * constructor - loads Model and other objects required in this controller
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('class_trainee_model', 'classTraineeModel');
+        $this->load->model('class_Model', 'classModel');
+        $this->load->model('company_model', 'companyModel');
+        $this->load->model('tenant_model', 'tenantModel');
+        $this->load->helper('common');
+        $this->load->helper('metavalues_helper');
+        $this->load->model('meta_values', 'meta');
+        $this->user = $this->session->userdata('userDetails');
+        $this->tenant_id = $this->session->userdata('userDetails')->tenant_id;
+    }
+
+    /*
+     * This function loads the initial static page for accounting.
+      */
+   public function index() {
+       $data['sideMenuData'] = fetch_non_main_page_content();
+        $data['page_title'] = 'Accounting';
+        $data['main_content'] = 'classtrainee/accounting';
+        $this->load->view('layout', $data);
+    }
+    
+    public function ssgapi_course(){
+        $data['sideMenuData'] = fetch_non_main_page_content();
+        $data['page_title'] = 'Accounting';
+        $data['main_content'] = 'ssgapi_course/course';
+        $this->load->view('layout', $data);
+    }
+}
