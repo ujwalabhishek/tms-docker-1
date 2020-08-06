24da04421e5673f58d97e443891cb2285a7b90d1
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-08-06 09:49:27 +0530

    ssg api integration

--- www/newtms/application/controllers/Ssgapi_course.php
+++ www/newtms/application/controllers/Ssgapi_course.php
@@ -83,7 +83,7 @@ class ssgapi_course extends CI_Controller {
 
         //print_r(json_decode($response));exit;
         
-        
+        echo $response_token->access_token;exit;
         
         //$result = file_get_contents($google_api_url);
 
