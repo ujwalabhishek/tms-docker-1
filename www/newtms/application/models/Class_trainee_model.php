e7a723ae6f4ecbcdb5ce935d3879df11fe442a4a
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-10-27 15:29:51 +0530

    everest invoice testing update

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -12914,6 +12914,20 @@ tup . first_name , tup . last_name, due.total_amount_due,due.subsidy_amount, ce.
         $tenant_id = $this->tenant_id;
 
         $invoice_id_tmp = get_max_lookup($lookup_table[$tenant_id]);
+        
+        if(strlen($invoice_id_tmp)== 1){
+            $invoice_id_tmp = '0000'.$invoice_id_tmp;
+        }elseif(strlen($invoice_id_tmp)== 2){
+            $invoice_id_tmp = '000'.$invoice_id_tmp;
+        }elseif(strlen($invoice_id_tmp)== 3){
+            $invoice_id_tmp = '00'.$invoice_id_tmp;
+        }elseif(strlen($invoice_id_tmp)== 4){
+            $invoice_id_tmp = '0'.$invoice_id_tmp;
+        }elseif(strlen($invoice_id_tmp)== 5){
+            $invoice_id_tmp = $invoice_id_tmp;
+        }else{
+            $invoice_id_tmp = $invoice_id_tmp;
+        }
 
         if($tenant_id == 'T17'){
             $invoice_id = $pre_fix_array[$tenant_id] .'-20'.date('y').'-'.$invoice_id_tmp;
