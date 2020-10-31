e7173aef38485660e8c3728452a4aa0e18871f41
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-10-31 15:42:53 +0530

    everest setup

--- www/newtms/assets/js/updatepayment.js
+++ www/newtms/assets/js/updatepayment.js
@@ -729,8 +729,8 @@ $(document).ready(function() {
                         $payrcd_table.append($html);
                     }
                     
-                   if(res.tenant.tenant_id='T17'){
-                       $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
+                   if(res.tenant.tenant_id='T17'){alert(res.tenant.tenant_id);
+                       $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
                         //$('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="NETS1">NETS</option>');
                     }else{
                         //$('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="GIRO1">GIRO</option><option value="NETS1">NETS</option>');
