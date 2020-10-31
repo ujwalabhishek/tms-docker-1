6c3db4d20b8f7f2cb2ae6815ccb28c29e2f7c7af
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-10-31 15:17:12 +0530

    everest setup

--- www/newtms/assets/js/updatepayment.js
+++ www/newtms/assets/js/updatepayment.js
@@ -517,7 +517,9 @@ $(document).ready(function() {
                     $('.common_pay').show();
                     if(data.tenant_id=='T17'){///added by shubhranshu for everest
                         $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="CHQ">Cheque</option>');
+                        $('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="NETS1">NETS</option>');
                     }else{
+                        $('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="GIRO1">GIRO</option><option value="NETS1">NETS</option>');
                         $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
                     }
                     
@@ -727,8 +729,14 @@ $(document).ready(function() {
                         $payrcd_table.append($html);
                     }
                     
-                   
-                    $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
+                   if(res.data.tenant_id='T17'){
+                       $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
+                        $('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="NETS1">NETS</option>');
+                    }else{
+                        $('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="GIRO1">GIRO</option><option value="NETS1">NETS</option>');
+                      $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="GIRO">GIRO</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>'); 
+                   }
+                    
                     
                 }
             });
