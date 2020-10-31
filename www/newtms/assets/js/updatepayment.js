e1e61d1b68e4c78b09aaeeb640991e6dd508756d
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-10-31 15:51:31 +0530

    everest setup

--- www/newtms/assets/js/updatepayment.js
+++ www/newtms/assets/js/updatepayment.js
@@ -520,7 +520,7 @@ $(document).ready(function() {
                         
                     }else{
                         
-                        $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
+                        $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="GIRO">GIRO</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
                     }
                     
                 } else {
@@ -731,9 +731,9 @@ $(document).ready(function() {
                     
                    if(res.data.tenant_id=='T17'){
                        $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="CHQ">Cheque</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>');
-                        //$('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="NETS1">NETS</option>');
+                        $('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option>');
                     }else{
-                        //$('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="GIRO1">GIRO</option><option value="NETS1">NETS</option>');
+                        $('#payment_type1').html('<option value="" selected="selected">Select</option><option value="CASH1">Cash</option><option value="CHQ1">Cheque</option><option value="GIRO1">GIRO</option><option value="NETS1">NETS</option>');
                       $('#payment_type').html('<option value="" selected="selected">Select</option><option value="CASH">Cash</option><option value="NETS">NETS</option><option value="CHQ">Cheque</option><option value="GIRO">GIRO</option><option value="SFC_SELF">SFC_SELF</option><option value="SFC_ATO">SFC_ATO</option>'); 
                    }
                     
