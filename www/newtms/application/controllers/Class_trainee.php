220c8e244be52a1019f480642bcf09bd0bb796ba
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-12-02 10:29:58 +0530

    activity log fix

--- www/newtms/application/controllers/Class_trainee.php
+++ www/newtms/application/controllers/Class_trainee.php
@@ -4066,8 +4066,33 @@ if (!empty($tenant_details->tenant_contact_num)) {
                             <li>Trim finger nails and remove nail polish</li>
                             '.$li.'
                         </ol>';
-                }else{
+                }elseif($tenant_details->tenant_id == 'T02'){
                         $data = 'Your seat has been booked. Please pay the class fees on or before the class start date.
+                         for <strong>' . $trainee . '</strong> for \'Course: ' . $courses->crse_name . ', Class: ' . $classes->class_name . ', Certificate Code: ' . $courseLevel . '\'<br><br>
+                        <strong>Class start date:</strong>
+                        ' . date('M d, Y h:i A', strtotime($classes->class_start_datetime)) . '
+                        <br><br>
+                         <strong>Class end date:</strong>
+                        ' . date('M d, Y h:i A', strtotime($classes->class_end_datetime)) . '
+
+                    <br>
+                    <br>
+                        <strong>Location:</strong>
+                        ' . $ClassLoc . '<br><br>
+                        <strong>Contact Details: </strong>
+                        ' . $contact_details . ' <br>
+                    <br>
+                        <strong>Remark *: </strong>
+                            <ol>
+                                <li> All participants please bring along their photo ID card with either their Nric/Fin number stated upon class date.</li>
+                                <li>Trim finger nails and remove nail polish</li>
+                                <li>Report at center at 8:30 AM to register for class</li>
+                                <li>In the event of unforeseen circumstances (example: SkillsFuture Credit website is down for maintenance, etc),
+Cash payment has to be collected from Candidate and Xprienz Pte Ltd will assist in making the appeal for them.</li>
+                                 <li>Dress code : Covered shoes</li>
+                            </ol>';
+                }else{
+                    $data = 'Your seat has been booked. Please pay the class fees on or before the class start date.
                          for <strong>' . $trainee . '</strong> for \'Course: ' . $courses->crse_name . ', Class: ' . $classes->class_name . ', Certificate Code: ' . $courseLevel . '\'<br><br>
                         <strong>Class start date:</strong>
                         ' . date('M d, Y h:i A', strtotime($classes->class_start_datetime)) . '
