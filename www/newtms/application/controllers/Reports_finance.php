6fb8a27ab935e9aeb41f775c8adbca4fefe22020
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-12-02 09:33:29 +0530

    activity logs

--- www/newtms/application/controllers/Reports_finance.php
+++ www/newtms/application/controllers/Reports_finance.php
@@ -1361,7 +1361,7 @@ class Reports_finance extends CI_Controller {
 
         
         
-        if (!empty($module)) {
+        
             
             $this->db->cache_on();
             $tabledata = $this->activitylog->get_activity_list_by_tenant_id($tenant_id, $records_per_page, $offset, $field, $order_by, $module, $user_id, $com_id, $invid, $inv_taxcode, $crs, $cls_id, $cls_name, $account_type, $pass);
@@ -1376,7 +1376,7 @@ class Reports_finance extends CI_Controller {
 
         $data['tabledata'] = $tabledata;
         
-        }
+        
         
         $data['sort_order'] = $order_by;
 
