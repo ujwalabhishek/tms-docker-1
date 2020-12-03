14d8640f3b0a92b527bea7246aec929e1e5c9a55
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-12-03 10:31:42 +0530

    activity log fix

--- www/newtms/application/models/Activity_log_model.php
+++ www/newtms/application/models/Activity_log_model.php
@@ -206,7 +206,7 @@ class Activity_Log_Model extends CI_Model {
                 $this->db->limit($limit, $limitvalue);
             }
             $query = $this->db->get();
-//            echo $this->db->last_query();
+            echo $this->db->last_query();exit;
             return $query->result_array();
 
     }
