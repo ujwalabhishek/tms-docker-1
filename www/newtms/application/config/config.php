879e854b0ddb2ac06055fd1c84f707e5991a1fbf
Shubhranshu Panda <shubhranshu@biipbyte.com>
2020-12-29 15:21:13 +0530

    wsq tcs

--- www/newtms/application/config/config.php
+++ www/newtms/application/config/config.php
@@ -522,5 +522,5 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
-//$config['wsq_courses'] = array('95', '115', '67', '66', '65', '98', '100', '97', '101', '102', '103', '127','129', '128', '132','133','134', '135', '136', '137', '130', '131','158','159','162',164);// wsq courses added by shubhranshu
+$config['wsq_courses'] = array('95', '115', '67', '66', '65', '98', '100', '97', '101', '102', '103', '127','129', '128', '132','133','134', '135', '136', '137', '130', '131','158','159','162',164);// wsq courses added by shubhranshu
 $config['wsq_courses'] = array();
