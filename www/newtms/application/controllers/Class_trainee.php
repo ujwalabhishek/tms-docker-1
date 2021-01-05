8981f9db7812972f85df40b2d99ec79b50f1a7c7
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-01-05 17:25:19 +0530

    everest invoice modification

--- www/newtms/application/controllers/Class_trainee.php
+++ www/newtms/application/controllers/Class_trainee.php
@@ -3454,7 +3454,7 @@ class Class_Trainee extends CI_Controller {
         //print_r($result);exit;
        
         $this->load->helper('pdf_reports_helper');
-        if(empty($result) || $result=''){///for enrol invoice table data
+        if(empty($result)){///for enrol invoice table data
             echo "A ".$id.'--'.$inv;print_r($result);exit;exit;
             $res = $this->classtraineemodel->check_enrol_invoice_compind($id,$inv);
             if($res->inv_type == "INVINDV"){
@@ -3463,7 +3463,7 @@ class Class_Trainee extends CI_Controller {
                 $this->export_company_generate_invoice($id);
             }
         }else{////for view table data deleted invoice
-            print_r($result);exit;
+            echo "B ".$id.'--'.$inv;print_r($result);exit;
             if(($result->inv_type!="INVINDV") && (!empty($result->company_id))){
                 $data=  json_decode($result->invoice_details);
                 generate_company_pdf_invoice_all($data);
