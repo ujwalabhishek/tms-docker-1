c5ff65b43baa831f7ca34216c4f82b4753410c82
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-01-06 12:45:43 +0530

    everest invoice modification

--- www/newtms/application/views/reports/invoice_reg_list.php
+++ www/newtms/application/views/reports/invoice_reg_list.php
@@ -6,7 +6,7 @@
 
 </script>
 
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/invoice_reg_list.js?1.0356576"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/invoice_reg_list.js?1.03565766666"></script>
 
 <div class="col-md-10">
 
