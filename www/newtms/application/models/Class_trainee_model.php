4a3adf956c5d38eeea927312db1e97c16f2e7df5
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-01-11 22:45:42 +0530

    everest invoice modification

--- www/newtms/application/models/Class_trainee_model.php
+++ www/newtms/application/models/Class_trainee_model.php
@@ -14448,7 +14448,7 @@ tup . first_name , tup . last_name, due.att_status, due.total_amount_due,due.sub
  
         }
 $this->db->get()->row();
-echo $this->db_last_query();;exit;
+echo $this->db->last_query();;exit;
         //return $this->db->get()->row();
     }
 
