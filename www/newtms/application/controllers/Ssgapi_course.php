2f291699a9e968d074e7dd88445cd49a3cd60fc5
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-02-15 19:25:48 +0530

    tpg gateway testing

--- www/newtms/application/controllers/Ssgapi_course.php
+++ www/newtms/application/controllers/Ssgapi_course.php
@@ -197,23 +197,23 @@ class ssgapi_course extends CI_Controller {
         $keyfile = base_url()."assets/certificates/key.key";
         $url = "https://uat-api.ssg-wsg.sg/courses/runs/$query_string";
         $requestXml = "requestData";
-echo $url;exit;
-//        $ch = curl_init(); 
-//        curl_setopt($ch, CURLOPT_URL, $url); 
-//        curl_setopt($ch, CURLOPT_VERBOSE, 1); 
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1); 
-//        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1); 
-//        curl_setopt($ch, CURLOPT_FAILONERROR, 1); 
-//        curl_setopt($ch, CURLOPT_SSLCERT, $pemfile); 
-//        curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM'); 
-//        curl_setopt($ch, CURLOPT_SSLKEY, $keyfile); 
-//        curl_setopt($ch, CURLOPT_POST, 1);
-//        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
-//        //curl_setopt($ch, CURLOPT_POSTFIELDS, $requestXml);
-//        print_r($ch);exit;
-//        $ret = curl_exec($ch);
-//        print_r($ret);exit;
+
+        $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL, $url); 
+        curl_setopt($ch, CURLOPT_VERBOSE, 1); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 1); 
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1); 
+        curl_setopt($ch, CURLOPT_SSLCERT, $pemfile); 
+        curl_setopt($ch, CURLOPT_SSLCERTTYPE, 'PEM'); 
+        curl_setopt($ch, CURLOPT_SSLKEY, $keyfile); 
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/xml'));
+        //curl_setopt($ch, CURLOPT_POSTFIELDS, $requestXml);
+        
+        $ret = curl_exec($ch);
+        print_r($ret);exit;
 
     }
 }
