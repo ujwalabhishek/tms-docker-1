58f94b702ddf918219ab83f333a897adf26e9402
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-02-16 20:29:57 +0530

    tpg gateway testing

--- www/newtms/application/controllers/Ssgapi_course.php
+++ www/newtms/application/controllers/Ssgapi_course.php
@@ -385,12 +385,7 @@ class ssgapi_course extends CI_Controller {
         CURLOPT_SSLCERTTYPE => 'PEM', 
         CURLOPT_SSLKEY => $keyfile, 
         CURLOPT_POSTFIELDS, $dat, 
-        CURLOPT_HTTPHEADER => array(
-       "Authorization:  ",
-       "Cache-Control: no-cache",
-       "Content-Type: application/json"
-      
-        ),
+        CURLOPT_HTTPHEADER => $headers
       ));
         
         
