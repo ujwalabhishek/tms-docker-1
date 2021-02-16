f9029c62901cdfb2873cb42b722ab5a1621b25b3
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-02-16 19:04:53 +0530

    tpg gateway testing

--- www/newtms/application/controllers/Ssgapi_course.php
+++ www/newtms/application/controllers/Ssgapi_course.php
@@ -197,7 +197,10 @@ class ssgapi_course extends CI_Controller {
         $keyfile = base_url()."assets/certificates/key.key";
         $url = "https://uat-api.ssg-wsg.sg/courses/runs/$query_string";
         $requestXml = "requestData";
-
+        $headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg';
+        $headers[] = 'Connection: Keep-Alive';
+        $headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
+        $user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; Media Center PC 4.0)';
 //        $curl = curl_init(); 
 //        curl_setopt($curl, CURLOPT_URL, $url); 
 //        curl_setopt($curl, CURLOPT_VERBOSE, 1); 
@@ -208,6 +211,8 @@ class ssgapi_course extends CI_Controller {
 //        curl_setopt($curl, CURLOPT_SSLCERT, $pemfile); 
 //        curl_setopt($curl, CURLOPT_SSLCERTTYPE, 'PEM'); 
 //        curl_setopt($curl, CURLOPT_SSLKEY, $keyfile); 
+//        curl_setopt($process, CURLOPT_USERAGENT, $user_agent);
+//        curl_setopt($process, CURLOPT_HTTPHEADER, $headers);
 //        curl_setopt($curl, CURLOPT_POST, 1);
 //        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
 //        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Cache-Control: no-cache','Host: uat-api.ssg-wsg.sg'));
@@ -231,13 +236,10 @@ class ssgapi_course extends CI_Controller {
         CURLOPT_SSLCERTTYPE => 'PEM',
         CURLOPT_SSLKEY => $keyfile,
         CURLOPT_SSL_VERIFYHOST => 'https://uat-api.ssg-wsg.sg',
-        CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_HTTPHEADER => array(
-       
-       "Cache-Control: no-cache",
-       "Content-Type: application/json",
-       "Host: https://uat-api.ssg-wsg.sg"
-        ), 
+        CURLOPT_USERAGENT => $user_agent,
+        CURLOPT_HTTPHEADER => $headers,
+        CURLOPT_CUSTOMREQUEST => "GET"
+        
       ));
          $response = curl_exec($curl);
 
