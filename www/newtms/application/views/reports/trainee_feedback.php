bc04cc1e56a1c284ad66a809e39f636c250a9a79
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-04-23 11:38:29 +0530

    trainee feeback

--- www/newtms/application/views/reports/trainee_feedback.php
+++ www/newtms/application/views/reports/trainee_feedback.php
@@ -16,7 +16,7 @@ if (empty($class_id)) {
     var class_id = <?php echo $class_id ?>;
     var course_id = <?php echo $course_id ?>;
 </script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/trainee_feedback_report.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/trainee_feedback_report.js?0.001"></script>
 
 <div class="col-md-10">
     <?php
--- www/newtms/assets/js/trainee_feedback_report.js
+++ www/newtms/assets/js/trainee_feedback_report.js
@@ -205,6 +205,7 @@ $(document).ready(function() {
                     }
                    
                 });
+                $('#rating').val
                 $.each(trainee, function(i, item) {
                     //$('#rating option[value="' + item.trainee_feedback_rating + '"]').attr("selected", "selected"); 
                     $('#rating').val(item.trainee_feedback_rating);
