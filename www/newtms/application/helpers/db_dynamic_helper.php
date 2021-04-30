9ca8a302523f6f762add16e4b330177e84065d3f
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-04-30 14:57:59 +0530

    server issue check

--- www/newtms/application/helpers/db_dynamic_helper.php
+++ www/newtms/application/helpers/db_dynamic_helper.php
@@ -4,8 +4,8 @@ function switch_db_dynamic($name_db)
 {
     $config_app['dsn'] = '';
     $config_app['hostname'] = '172.18.0.2';
-    $config_app['username'] = 'root';
-    $config_app['password'] = 'tms#8938480*';
+    $config_app['username'] = 'biipmico_tms_master';
+    $config_app['password'] = 'ksj784382*879#';
     $config_app['database'] = $name_db;
     $config_app['dbdriver'] = 'mysqli';
     $config_app['dbprefix'] = '';
@@ -30,8 +30,8 @@ function fetch_dynamic_db_details(){
     if($host == 'xprienz.net'){
         $object = new stdClass();
         $object->tenant_db_name = 'biipmico_tms_masterdata';
-        $object->tenant_db_user = 'root';
-        $object->tenant_db_password = 'tms#8938480*';
+        $object->tenant_db_user = 'biipmico_tms_master';
+        $object->tenant_db_password = 'ksj784382*879#';
         
         return $object;
     }
