67069a6e042387025ae83186a1f87d7980ea7c7a
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-06-01 19:16:51 +0530

    create course run- ssg/tpg

--- www/newtms/application/views/tp_gateway/get_courserun.php
+++ www/newtms/application/views/tp_gateway/get_courserun.php
@@ -297,26 +297,26 @@
         <tbody>
             <?php 
             foreach($sessions as $session){
-                echo $session['session_type_id'];exit;
-               if($session->session_type_id != 'BRK'){
+
+               if($session['session_type_id'] != 'BRK'){
             ?>
             <tr width="20%">                        
                 <td class="td_heading" width="20%">Session Start Date:<span class="required">*</span></td>
-                <td><label class="label_font"><?php echo date('Ymd', strtotime($session->class_date)) ?? $this->input->post('sess_start_time'); ?></label></td>
+                <td><label class="label_font"><?php echo date('Ymd', strtotime($session['class_date'])) ?? $this->input->post('sess_start_time'); ?></label></td>
                 <td class="td_heading">Session End Date:<span class="required">*</span></td>
                 <td><label class="label_font"></label><?php echo date('Ymd', strtotime($session->class_date)) ?? $this->input->post('sess_end_time'); ?></td>
             </tr>
 
             <tr>                        
                 <td class="td_heading">Session Start Time:<span class="required">*</span></td>
-                <td><label class="label_font"><?php echo date('h:i A', strtotime($session->session_start_time)) ?? $this->input->post('sess_start_time'); ?></label></td>
+                <td><label class="label_font"><?php echo date('h:i A', strtotime($session['session_start_time'])) ?? $this->input->post('sess_start_time'); ?></label></td>
                 <td class="td_heading">Session End Time:<span class="required">*</span></td>
-                <td><label class="label_font"></label><?php echo date('h:i A', strtotime($session->session_end_time)) ?? $this->input->post('sess_end_time'); ?></td>
+                <td><label class="label_font"></label><?php echo date('h:i A', strtotime($session['session_end_time'])) ?? $this->input->post('sess_end_time'); ?></td>
                 
-                <input type="hidden" name="sess_start_time" value="<?php echo date('h:i', strtotime($session->session_start_time)) ?? $this->input->post('sess_start_time'); ?>" id="sess_start_time">
-                <input type="hidden" name="sess_end_time" value="<?php echo date('h:i', strtotime($session->session_end_time)) ?? $this->input->post('sess_end_time'); ?>" id="sess_end_time">
-                <input type="hidden" name="sess_start_date" value="<?php echo date('Ymd', strtotime($session->session_start_time)) ?? $this->input->post('sess_start_date'); ?>" id="sess_start_date">
-                <input type="hidden" name="sess_end_date" value="<?php echo date('Ymd', strtotime($session->session_end_time)) ?? $this->input->post('sess_end_date'); ?>" id="sess_end_date">
+                <input type="hidden" name="sess_start_time" value="<?php echo date('h:i', strtotime($session['session_start_time'])) ?? $this->input->post('sess_start_time'); ?>" id="sess_start_time">
+                <input type="hidden" name="sess_end_time" value="<?php echo date('h:i', strtotime($session['session_end_time'])) ?? $this->input->post('sess_end_time'); ?>" id="sess_end_time">
+                <input type="hidden" name="sess_start_date" value="<?php echo date('Ymd', strtotime($session['class_date'])) ?? $this->input->post('sess_start_date'); ?>" id="sess_start_date">
+                <input type="hidden" name="sess_end_date" value="<?php echo date('Ymd', strtotime($session['class_date'])) ?? $this->input->post('sess_end_date'); ?>" id="sess_end_date">
             </tr>
             <?php 
                }
