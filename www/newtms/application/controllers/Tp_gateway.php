b78627526f0414832b0b686ceb164161232e4dfe
Abdulla Nofal <nofal09@gmail.com>
2021-06-10 01:48:23 +0530

    Enrolment API for TPG

--- www/newtms/application/controllers/Tp_gateway.php
+++ www/newtms/application/controllers/Tp_gateway.php
@@ -1285,14 +1285,13 @@ class tp_gateway extends CI_Controller {
         $url = "https://" . TPG_DEV_URL . "/courses/runs";
         $response = $this->curl_request('POST', $url, $tpg_enrolment_json, $api_version);
         print_r($response);exit;
-        $tpg_response = json_decode($response);        
-
+        $tpg_response = json_decode($response);
         if ($tpg_response->status == 200) {
             //$tpg_course_run_id = $tpg_response->data->runs[0]->id;            
 
             $this->session->set_flashdata("success", "Created");
 
-            redirect('class_trainee?course_id=' . $this->input->post('class_course').'class='.$this->input->post('class_course'));
+            redirect('class_trainee?course_id=' . $this->input->post('courseId').'&class='.$this->input->post('classId'));
         } else {
             if ($tpg_response->status == 400) {
                 $this->session->set_flashdata('error', "Oops! Bad request!");
