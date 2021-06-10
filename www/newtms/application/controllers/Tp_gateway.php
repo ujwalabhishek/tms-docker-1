f523e77fa24094cb9fc16968710075feb343118e
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-06-10 17:12:56 +0530

    tpg course module -delete courserun

--- www/newtms/application/controllers/Tp_gateway.php
+++ www/newtms/application/controllers/Tp_gateway.php
@@ -1365,12 +1365,12 @@ class tp_gateway extends CI_Controller {
           
             }</script>";
         
-      
+      $tpg_enrolment_encodeds = "$tpg_enrolment_encoded";
         //print_r($tpg_enrolment_encoded);exit;
         $api_version = 'v1';
         //$url = "https://" . TPG_DEV_URL . "/tpg/enrolments";
         $url = "https://uat-api.ssg-wsg.sg/tpg/enrolments";
-        $request = $this->curl_request('POST', $url, $tpg_enrolment_encoded, $api_version);        
+        $request = $this->curl_request('POST', $url, $tpg_enrolment_encodeds, $api_version);        
 
         $tpg_enrolment_decoded = "<div id='out_a'></div>
             
