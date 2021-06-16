a7d1f48fdab99aa699e0115934eba3e6101c8e44
Abdulla Nofal <nofal09@gmail.com>
2021-06-17 01:07:43 +0530

    Create enrolment check

--- www/newtms/application/controllers/Tp_gateway.php
+++ www/newtms/application/controllers/Tp_gateway.php
@@ -1383,8 +1383,8 @@ class tp_gateway extends CI_Controller {
 
         $output = openssl_decrypt($request, $encrypt_method, $key, 0, $iv); // remove explicit Base64 decoding (alternatively set OPENSSL_RAW_DATA)
 
-        $tpg_response = json_decode($output);
-
+        $tpg_response = json_decode($output, true);
+        print_r($tpg_response); exit;
         if ($tpg_response->status == 200) {
             $data['enrolmentReferenceNumber'] = $enrolmentReferenceNumber;
             
