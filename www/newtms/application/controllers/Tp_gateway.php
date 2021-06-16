c2d9ec3b39ad005db184732a6012ef2a1b05dbbb
Abdulla Nofal <nofal09@gmail.com>
2021-06-17 01:04:38 +0530

    Create enrolment check

--- www/newtms/application/controllers/Tp_gateway.php
+++ www/newtms/application/controllers/Tp_gateway.php
@@ -1388,7 +1388,7 @@ class tp_gateway extends CI_Controller {
         if ($tpg_response->status == 200) {
             $data['enrolmentReferenceNumber'] = $enrolmentReferenceNumber;
             
-            echo $tpg_response->data->enrolment[0]->referenceNumber; exit;
+            echo $tpg_response->data->referenceNumber; exit;
             
             $data['referenceNumber'] = $tpg_response->data->enrolment[0]->referenceNumber;
             $data['status'] = $tpg_response->data->enrolment[0]->status;
