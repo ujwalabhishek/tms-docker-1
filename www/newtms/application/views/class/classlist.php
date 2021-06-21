4686c74b6069795c14cb33afbb67b9fe43cd01c4
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-06-21 13:15:07 +0530

    tpg course module -fetch ssg sessions

--- www/newtms/application/views/class/classlist.php
+++ www/newtms/application/views/class/classlist.php
@@ -117,7 +117,7 @@ $CI->load->model('class_model');
                             $crsrunid_js = $crsrunid_js;
                             $tpg_course_run_id_options[''] = 'All';
                         } else {
-                            $crsrunid_js = $crsrunid_js . ' disabled="disabled"';
+                            //$crsrunid_js = $crsrunid_js . ' disabled="disabled"';
                         }
                         foreach ($courseRunId as $k => $v) {
                             $tpg_course_run_id_options[$k] = $v;
