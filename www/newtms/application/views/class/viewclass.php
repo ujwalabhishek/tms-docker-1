b7562c7ef646cb80ea0faf3ddee92e10db643387
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-06-21 10:31:15 +0530

    tpg course module -delete courserun

--- www/newtms/application/views/class/viewclass.php
+++ www/newtms/application/views/class/viewclass.php
@@ -307,7 +307,7 @@ $role_array = array("COMPACT");
     </table>
 </div>
 <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <!-- Modal -->
