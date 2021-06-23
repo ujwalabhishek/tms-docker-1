a41452ed85ef8e1c69bd90e9107b86a303be9b28
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-06-23 19:30:11 +0530

    tpg assessment module- create assessment

--- www/newtms/application/views/class/tpg_assessments.php
+++ www/newtms/application/views/class/tpg_assessments.php
@@ -4,7 +4,7 @@
     $role_check = '<?php echo $this->data['user']->role_id; ?>';
     $tenant_id = '<?php echo $this->data['user']->tenant_id; ?>';
 </script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/tpg_assessment.js?11111.000"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/tpg_assessment.js?11111.0000"></script>
 <style>
     table td{
         font-size: 11px;
