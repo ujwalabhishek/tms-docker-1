dbb60ba9945ebfdba971043fb38057a820fe3236
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-06-25 00:18:55 +0530

    tpg assessment module- create assessment

--- www/newtms/assets/js/tpg_assessment.js
+++ www/newtms/assets/js/tpg_assessment.js
@@ -98,15 +98,15 @@ $(document).ready(function () {
             },
             success: function (res) {
                 json_data = $.parseJSON(res);
-                if (json_data != '') {alert(res);
+                if (json_data != '') {alert(res.status);
                     //if(json_data.status == 200){
                         
                    // }else{
                        // $('#viewsection').hide();
                     //}
-                   $.each(json_data.data.sessions, function(i, item) {
-                       $('#ssg_sess').append('<tr><td>'+(i+1)+'</td><td>'+item.id+'</td><td>'+item.startDate+'</td><td>'+item.endDate+'</td><td>'+item.startTime+'</td><td>'+item.endTime+'</td><td>'+item.attendanceTaken+'</td></tr>');
-                    });
+                  // $.each(json_data.data.sessions, function(i, item) {
+                   //    $('#ssg_sess').append('<tr><td>'+(i+1)+'</td><td>'+item.id+'</td><td>'+item.startDate+'</td><td>'+item.endDate+'</td><td>'+item.startTime+'</td><td>'+item.endTime+'</td><td>'+item.attendanceTaken+'</td></tr>');
+                   // });
                 }
             }
         });
