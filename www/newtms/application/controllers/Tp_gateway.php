835b2f572c89a63d074b22992e785897639e2384
Abdulla Nofal <nofal09@gmail.com>
2021-06-26 19:41:28 +0530

    TPG - Cancel Operation

--- www/newtms/application/controllers/Tp_gateway.php
+++ www/newtms/application/controllers/Tp_gateway.php
@@ -1665,9 +1665,7 @@ class tp_gateway extends CI_Controller {
             $traineeContactNumber = $this->input->post('traineeContactNumber');
             $traineeEmailAddress = $this->input->post('traineeEmailAddress');
             $feeDiscountAmount = $this->input->post('feeDiscountAmount');
-            $feeCollectionStatus = $this->input->post('feeCollectionStatus');
-            
-            echo "aaa - ".$feeCollectionStatus; exit;
+            $feeCollectionStatus = $this->input->post('feeCollectionStatus');                        
 
             if ($sponsorshipType != "Individual") {
                 $employerContactFullName = $this->input->post('employerContactFullName');
@@ -1730,6 +1728,8 @@ class tp_gateway extends CI_Controller {
                                           }
                                         }
                                       }';
+        
+        echo "aaa - ".$tpg_enrolment_json_data; exit;
 
         $encrypted_output = openssl_encrypt($tpg_enrolment_json_data, $encrypt_method, $key, 0, $iv); // remove explicit Base64 encoding (alternatively set OPENSSL_RAW_DATA)
 
