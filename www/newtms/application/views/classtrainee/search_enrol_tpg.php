05607d0bdff799f67b8282d368f3701cc3c68988
Abdulla Nofal <nofal09@gmail.com>
2021-06-26 20:19:56 +0530

    TPG - Cancel Operation

--- www/newtms/application/views/classtrainee/search_enrol_tpg.php
+++ www/newtms/application/views/classtrainee/search_enrol_tpg.php
@@ -4,7 +4,7 @@
     $role_check = '<?php echo $this->data['user']->role_id; ?>';
     $tenant_id = '<?php echo $this->data['user']->tenant_id; ?>';
     
-    var CLIENT_DATE_FORMAT = 'Y-m-d';       
+    var CLIENT_DATE_FORMAT = 'yyyy-mm-dd';            
 </script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/classtraineelist.js?0.0311111111111111"></script>
 <style>
