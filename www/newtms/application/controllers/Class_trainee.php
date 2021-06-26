873acf92cc050e98b562b362691d5036dab0156d
Abdulla Nofal <nofal09@gmail.com>
2021-06-26 13:02:08 +0530

    TPG - Search Enrolment API

--- www/newtms/application/controllers/Class_trainee.php
+++ www/newtms/application/controllers/Class_trainee.php
@@ -359,7 +359,7 @@ class Class_Trainee extends CI_Controller {
                                         },
                                         "parameters": {
                                           "page": 0,
-                                          "pageSize": 1
+                                          "pageSize": 20
                                         }
                                       }';
 
