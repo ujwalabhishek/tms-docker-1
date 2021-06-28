54147face05d6e2244eba80542d6fabb305ec47a
Abdulla Nofal <nofal09@gmail.com>
2021-06-28 21:14:44 +0530

    Enrolment

--- www/newtms/application/views/classtrainee/classtraineelist.php
+++ www/newtms/application/views/classtrainee/classtraineelist.php
@@ -393,7 +393,7 @@
                                         <span style="color:red"><i>This enrolment is Cancelled.</i></span>
                                         <?php } ?>
                                         <?php if($enrolmentStatus == "Confirmed") {?>
-                                        <a href="javascript:;" rel="modal:open" href="#edit_enrolment" data-class="' . $row['class_id'] . '" data-course="' . $row['course_id'] . '" data-user="' . $row['user_id'] . '"><button type="button" class="btnblue">Edit Enrolment</button></a>
+                                        <a href="javascript:;" rel="modal:open" href="#edit_enrolment" data-class="<?php echo $tpgClassId; ?>" data-course="<?php echo $tpgCourseId; ?>" data-user="<?php echo $tmsUserId; ?>" data-paymentstatus="<?php echo $tmsPaymentStatus; ?>" data-enrolrefnum="<?php echo $enrolmentReferenceNumber; ?>"><button type="button" class="btnblue">Edit Enrolment</button></a>
                                         <br>
                                         <a href="javascript:;" rel="modal:open" href="#abd"><button type="button" class="btnblue">Update Fee</button></a>
                                         <?php } ?>
