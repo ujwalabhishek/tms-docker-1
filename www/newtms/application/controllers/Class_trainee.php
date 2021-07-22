192f8b2ba723a087b512d676c7c42c8b837b2932
Abdulla Nofal <nofal09@gmail.com>
2021-07-22 17:59:16 +0530

    Re-schedule

--- www/newtms/application/controllers/Class_trainee.php
+++ www/newtms/application/controllers/Class_trainee.php
@@ -1644,8 +1644,7 @@ class Class_Trainee extends CI_Controller {
                         $data['active_enroll_course_id'] = get_course_id($active_enroll_class);
                         $data['active_enroll_lock_att_status'] = get_active_class_att_status($active_enroll_class);
                         //Added by abdulla
-                        $data['active_enroll_eid_no'] = get_eid_id($active_enroll_class);
-                        echo print_r($data['active_enroll_eid_no']); exit;
+                        $data['active_enroll_eid_no'] = get_eid_id($active_enroll_class);                        
                         $data['active_enroll_tpg_status'] = get_active_class_tpg_status($active_enroll_class);
                         $active_enroll_class_id = get_class_id($active_enroll_class);
                         //$reschedule_enroll_class = $this->classtraineemodel->get_reschedule_class_enrol($tenant_id, '', $active_enroll_class_id);////commented by shubhranshu
@@ -5439,7 +5438,7 @@ function get_eid_id($data_arr) {
     if (!empty($data_arr)) {
         $arr = array();
         foreach ($data_arr as $row) {
-            $arr[$row->eid_number] = $row->eid_number;
+            $arr[$row->class_id] = $row->eid_number;
         }
         return $arr;
     }
@@ -5455,7 +5454,7 @@ function get_active_class_tpg_status($data_arr) {
     if (!empty($data_arr)) {
         $arr = array();
         foreach ($data_arr as $row) {
-            $arr[$row->tpg_enrolment_status] = $row->tpg_enrolment_status;
+            $arr[$row->class_id] = $row->tpg_enrolment_status;
         }
         return $arr;
     }
