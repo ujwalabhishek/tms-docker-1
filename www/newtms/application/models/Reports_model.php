abe63f03d304ec398af9a69720c280a33e0ec3ae
Shubhranshu Panda <shubhranshu@biipbyte.com>
2021-07-27 21:20:13 +0530

    attn auto load

--- www/newtms/application/models/Reports_model.php
+++ www/newtms/application/models/Reports_model.php
@@ -3649,7 +3649,7 @@ SELECT  {$calc_rows} c.crse_name,
         ' ' as 'EmployerPhoneAreaCode',
         COALESCE(cm.comp_phone,'') as 'EmployerPhone',
         COALESCE(cm.comp_email,'') as 'EmployerContactEmail',
-        ei.total_inv_discnt as 'CourseFeeDiscountAmount',
+        (ceil((due.class_fees * due.discount_rate))/100) as 'CourseFeeDiscountAmount',
         (CASE 
          WHEN ce.payment_status like 'PARTPAID' THEN 'Partial Payment' 
          WHEN ce.payment_status like 'PAID' THEN 'Full Payment' 
