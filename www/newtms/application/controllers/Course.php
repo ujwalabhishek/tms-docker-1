168bbe2a0fd7014a5f241e052aa3755a0f7915e6
Abdulla Nofal <nofal09@gmail.com>
2021-08-12 16:46:10 +0530

    Modification for Non - TPG Course

--- www/newtms/application/controllers/Course.php
+++ www/newtms/application/controllers/Course.php
@@ -1139,7 +1139,8 @@ class Course extends CI_Controller {
             $tpg_response = json_decode($request);
 
             if ($tpg_response->status == 200) {
-                //echo "<pre>".print_r($tpg_response, true)."</pre>";                
+                //echo "<pre>".print_r($tpg_response, true)."</pre>";
+                $data['tpg_response'] = $tpg_response;
                 $data['course_name_val'] = $tpg_response->data->courses[0]->title;
                 $data['external_reference_number_val'] = $tpg_response->data->courses[0]->externalReferenceNumber;
                 $data['course_duration_val'] = $tpg_response->data->courses[0]->totalTrainingDurationHour;
