7d4f32e2474a4841e61bb94bdd78060124fb751b
Abdulla Nofal <nofal09@gmail.com>
2021-08-17 17:35:28 +0530

    Modification for non - tpg course at class level.

--- www/newtms/application/controllers/Classes.php
+++ www/newtms/application/controllers/Classes.php
@@ -384,10 +384,10 @@ class Classes extends CI_Controller {
                 $this->session->set_flashdata("error", "You cannot Edit a 'Deleted' class.");
                 return redirect("classes");
             } else {
-                echo print_r($course_id); exit;
-                $course_details = $this->coursemodel->get_course_detailse($course_id);
                 
+                $course_details = $this->coursemodel->get_course_detailse($course_id);                
                 $data['tpg_crse'] = $course_details->tpg_crse;
+                echo print_r($data['tpg_crse']); exit;
                 $data['classid'] = $class_id;
                 $data['coursename'] = $this->coursemodel->course_name($course_id);
                 $data['classes'] = $this->classmodel->get_course_class_for_edit($tenant_id, $course_id);
