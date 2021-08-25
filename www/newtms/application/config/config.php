cfb85023b81d51d6060a9b3821252201cc43be73
Abdulla Nofal <nofal09@gmail.com>
2021-08-25 14:34:48 +0530

    Testing of production of API's

--- www/newtms/application/config/config.php
+++ www/newtms/application/config/config.php
@@ -528,10 +528,10 @@ $config['wsq_courses'] = array('95','115','67','66','65','98','100','97','101','
 $config['TPG_KEY_T02'] = "DLTmpjTcZcuIJEYixeqYU4BvE+8Sh4jDtDBDT3yA8D0=";
 
 //Key for FLC Tenant
-$config['TPG_KEY_T04'] = "DLTmpjTcZcuIJEYixeqYU4BvE+8Sh4jDtDBDT3yA8D0=";
+$config['TPG_KEY_T04'] = "n8DfGN39ME6bn333gILFDUeyDeXU9vZMZXnCdCQ65EA=";
 
 //Key for SSI Tenant
 $config['TPG_KEY_T18'] = "QFyCysgjUNQ08wBWi07/1WkEj0+u1oyq5o7wfdcfrgI=";
 
 //Key for FONDA GLOBAL Tenant
-$config['TPG_KEY_T25'] = "DLTmpjTcZcuIJEYixeqYU4BvE+8Sh4jDtDBDT3yA8D0=";
+$config['TPG_KEY_T25'] = "1FB65Prs/602t9KV3vFs1Ogl7DiQoY63WP/QtjbDF3I=";
