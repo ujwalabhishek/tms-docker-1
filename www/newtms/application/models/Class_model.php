2aa9ea391b173af8a836ca4a9945b2b77465866a
Abdulla Nofal <nofal09@gmail.com>
2021-09-03 22:49:27 +0530

    Bug fix for copy class

--- www/newtms/application/models/Class_model.php
+++ www/newtms/application/models/Class_model.php
@@ -2536,7 +2536,7 @@ class Class_Model extends CI_Model {
                 AND ce.training_score !='' $str
                 AND date(cc.class_end_datetime) <= '$today_date'";
         $result = $this->db->query($sql)->result();
-        echo $this->db->last_query();exit;
+        //echo $this->db->last_query();exit;
         return $result[0];
     }
 
