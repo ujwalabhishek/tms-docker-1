f4cbaaef243fcff8db268628917e0972241d046e
Abdulla Nofal <nofal09@gmail.com>
2021-09-04 19:49:42 +0530

    Bug Fixing

--- www/newtms/application/config/tms_routes.php
+++ www/newtms/application/config/tms_routes.php
@@ -79,13 +79,7 @@ $tms_route['RPTFIN']['ops'] = array(
     'SLSUMBYMN' => 'sales_summary_monthwise',
     'ACTLOG' => 'activity_log',
     'TMSRPTS' => 'tms_report',
-    'SALREP' => 'sales_report',
     'INVADTRAIL' => 'invoice_audit_trail'); //Added for new Invoice Audit Trail Report
-    //
-    //////---ssg api course
-$tms_route['SSGCRSE']['controller_name'] = 'ssgapi_course';
-$tms_route['SSGCRSE']['ops'] = array(
-    'LSTSRCSSGCRSE' => 'list_search_course');
 //------------------------ Manage Tenant -------------------------------------
 $tms_route['MNGTENT']['controller_name'] = 'manage_tenant';
 $tms_route['MNGTENT']['ops'] = array(
