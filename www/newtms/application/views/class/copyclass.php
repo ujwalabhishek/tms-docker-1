f19ce39874b606a4a8ed58113d615d47a5f421e3
Abdulla Nofal <nofal09@gmail.com>
2021-09-15 19:03:47 +0530

    Copy class

--- www/newtms/application/views/class/copyclass.php
+++ www/newtms/application/views/class/copyclass.php
@@ -235,7 +235,7 @@ if (!empty($tax_error)) {
                                 </td>
                             </tr>
                             <tr>
-                                <td colspan="3" class="td_heading">  
+                                <td colspan="6" class="td_heading">  
                                     <?php
                                     if ($class->wheel_chair_access == '1') {
                                         $checked_wheel = 'checked';
