0d6aab367978c498004b8c7193157ddcd3501214
Abdulla Nofal <nofal09@gmail.com>
2021-09-26 00:17:23 +0530

    Update TPG Class - FIX

--- www/newtms/application/models/Class_model.php
+++ www/newtms/application/models/Class_model.php
@@ -696,7 +696,7 @@ class Class_Model extends CI_Model {
      * this function to get all class schedules
      */
     public function get_all_class_schedule($tenant_id, $cid) {
-        $result = $this->db->query("select class_date, session_type_id, session_start_time,session_end_time
+        $result = $this->db->query("select class_date, session_type_id, session_start_time,session_end_time,tpg_session_id,mode_of_training
                 from class_schld where tenant_id='$tenant_id' and class_id='$cid'
                 order by class_date DESC, session_start_time ASC");
         return $result->result_array();
