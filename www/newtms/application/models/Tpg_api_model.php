2b34f52c41618e669620a2acdbfd902a54c354f1
Abdulla Nofal <nofal09@gmail.com>
2021-09-26 02:31:22 +0530

    Update TPG Class - FIX

--- www/newtms/application/models/Tpg_api_model.php
+++ www/newtms/application/models/Tpg_api_model.php
@@ -1161,7 +1161,7 @@ class Tpg_api_Model extends CI_Model {
                                   "courseReferenceNumber": "' . $retun[ref_no] . '"
                                 }
                             }';
-        //print_r($tpg_course_run_json);exit;
+        print_r($tpg_course_run_json);exit;
         $api_version = 'v1.3';
         $url = "https://" . $retun[domain] . "/courses/runs/". $tpg_course_run_id;
         $response = $this->curl_request('POST', $url, $tpg_course_run_json, $api_version);        
