acb2420f6480699d7261cbbbdc252b68ab63c5c9
Abdulla Nofal <nofal09@gmail.com>
2021-09-26 16:38:26 +0530

    Update TPG Class - FIX

--- www/newtms/application/views/class/editclass_tpg.php
+++ www/newtms/application/views/class/editclass_tpg.php
@@ -666,7 +666,7 @@ if (!empty($tax_error)) {
                             </td>
                         </tr>
                         <tr>
-                            <td colspan="5" class="td_heading">  
+                            <td colspan="6" class="td_heading">  
                                 <?php
                                 $wheel_chair_checked = ($class->wheel_chair_access == 1) ? true : false;
                                 $wheel_chair_accessible = array(
