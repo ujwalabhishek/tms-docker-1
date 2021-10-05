06fc8e53b73769a176c3c171c54cd7455fe0828c
Abdulla Nofal <nofal09@gmail.com>
2021-10-05 14:02:13 +0530

    View session - check

--- www/newtms/application/models/Tpg_api_model.php
+++ www/newtms/application/models/Tpg_api_model.php
@@ -1093,7 +1093,7 @@ class Tpg_api_Model extends CI_Model {
                                   "courseReferenceNumber": "' . $retun[ref_no] . '"
                                 }
                             }';
-        //print_r($tpg_course_run_json);exit;
+        print_r($tpg_course_run_json);exit;
                     
         $api_version = 'v1.3';
         $url = "https://" . $retun[domain] . "/courses/runs/". $tpg_course_run_id;
