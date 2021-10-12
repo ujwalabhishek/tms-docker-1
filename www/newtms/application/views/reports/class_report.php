d6b83822e2c1fbc396feca6fd56a2ff17665e021
Abdulla Nofal <nofal09@gmail.com>
2021-10-13 00:06:50 +0530

    Fonda Global - Report

--- www/newtms/application/views/reports/class_report.php
+++ www/newtms/application/views/reports/class_report.php
@@ -130,7 +130,20 @@
                             <td><?php echo $row->TraineeID ;?></td>
                             <td><?php echo $row->TraineeName ;?></td>
                             <td><?php echo $row->Gender;?></td>
-                            <td><?php echo ($row->Nationality) ? get_catname_by_parm($row->Nationality) : ' - '; ?></td>
+                            <td>
+                                <?php 
+                                    $nationalityLabel = '';
+                                    $nationality = fetch_metavalues_by_category_id(Meta_Values::NATIONALITY);
+                                    foreach ($nationality as $item):
+                                        if ($item['parameter_id'] == $row->Nationality) {
+                                            $nationalityLabel = $item['category_name'];
+                                            break;
+                                        }
+                                    endforeach;
+
+                                    echo ($nationalityLabel);
+                                ?>
+                            </td>
                             <td><?php echo $row->DateofBirth;?></td>
                             <td><?php echo ($row->Race) ? get_catname_by_parm($row->Race) : ' - '; ?></td>
                             <td><?php echo $row->TraineePhone;?></td>
