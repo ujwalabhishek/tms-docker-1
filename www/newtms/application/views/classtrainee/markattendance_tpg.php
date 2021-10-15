dc5030d103ca6e56184c94fbe792df8676ab5de8
Abdulla Nofal <nofal09@gmail.com>
2021-10-15 10:46:51 +0530

    ARIUM - TMS Setup

--- www/newtms/application/views/classtrainee/markattendance_tpg.php
+++ www/newtms/application/views/classtrainee/markattendance_tpg.php
@@ -104,8 +104,8 @@
     <?php ?>
     <div class="bs-example">
         <div class="table-responsive">
-            <div style="clear:both;"></div>
-            <b>No. of Trainees in the class :</b><span class="nric_count"> <b><?php echo !empty($nric_count) ? $nric_count : '-'; ?></b></span><br>
+            <b>No. of Trainees in the class :</b><span class="nric_count"> <b><?php echo !empty($nric_count) ? $nric_count : '-'; ?></b></span>
+            <div style="clear:both;"></div>            
             <div style='color:grey'><b>Attn Status Code:</b> 1-Confirmed,2-Unconfirmed,3-Rejected ,4-TP Voided</div>
             <div style='color:grey'><b>ID Type:</b> SB-Singapore Blue Identification Card, SP-Singapore Pink Identification Card, SO-Fin/Work Permit/SAF 11B, OT-Others</div>
             <table class="table table-striped">
