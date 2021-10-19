32a5367e5d414fd33ba8e3e6890a1571347465e9
Abdulla Nofal <nofal09@gmail.com>
2021-10-20 01:56:46 +0530

    Bulk Enrollment -  TPG

--- www/newtms/application/controllers/Class_trainee.php
+++ www/newtms/application/controllers/Class_trainee.php
@@ -1626,11 +1626,11 @@ class Class_Trainee extends CI_Controller {
             $totalrows = $this->classtraineemodel->get_all_classtrainee_count_by_tenant_id($tenant_id, $course, $class, $class_status, $search_select, $taxcode_id, $trainee_id, $company_id, $eid);
             $new_tabledata = array();
             $role_array = array("TRAINER", "COMPACT", "SLEXEC");
-            $data['tpg_course_run_id'] = $tabledata['tpg_course_run_id'];
-            $data['reference_num'] = $tabledata['reference_num'];
-            $data['course_id'] = $tabledata['course_id'];
-            $data['class_id'] = $tabledata['class_id'];
-                        echo print_r($tabledata, true); 
+            $data['tpg_course_run_id'] = $tabledata[0]['tpg_course_run_id'];
+            $data['reference_num'] = $tabledata[0]['reference_num'];
+            $data['course_id'] = $tabledata[0]['course_id'];
+            $data['class_id'] = $tabledata[0]['class_id'];
+            //echo print_r($tabledata, true); 
             foreach ($tabledata as $k => $row) {
                 if ($row['enrolment_mode'] == 'COMPSPON') {
                     if ($row['company_id'][0] == 'T') {
