d5fe198848a842a2764f1656b12c82587efdc2f6
Abdulla Nofal <nofal09@gmail.com>
2021-11-19 03:20:34 +0530

    Bulk Enrolment

--- www/newtms/application/controllers/Tp_gateway.php
+++ www/newtms/application/controllers/Tp_gateway.php
@@ -2164,7 +2164,7 @@ class tp_gateway extends CI_Controller {
                         padding: CryptoJS.pad.Pkcs7
                     });
                     var encrypted = CryptoJS.enc.Base64.stringify(cipher.ciphertext);                    
-                    alert('encrypted'+$aaa);
+                    alert(encrypted);
               }
               </script>";
         
